c81ec30af1038e0282129a3147d6d4bc880380be
George Gastaldi <gegastaldi@gmail.com>
2014-12-09 14:19:39 -0200

    More stuff done

--- src/main/java/org/jboss/forge/rest/ui/RestUIContext.java
+++ src/main/java/org/jboss/forge/rest/ui/RestUIContext.java
@@ -1,22 +1,38 @@
 package org.jboss.forge.rest.ui;
 
+import org.jboss.forge.addon.resource.Resource;
 import org.jboss.forge.addon.ui.UIProvider;
 import org.jboss.forge.addon.ui.context.AbstractUIContext;
 import org.jboss.forge.addon.ui.context.UISelection;
 import org.jboss.forge.addon.ui.util.Selections;
 
-public class RestUIContext extends AbstractUIContext {
+public class RestUIContext extends AbstractUIContext
+{
+   private final Resource<?> selection;
+   private final RestUIProvider provider = new RestUIProvider();
 
-	private RestUIProvider provider = new RestUIProvider();
+   public RestUIContext()
+   {
+      this.selection = null;
+   }
 
-	@Override
-	public <SELECTIONTYPE> UISelection<SELECTIONTYPE> getInitialSelection() {
-		return Selections.emptySelection();
-	}
+   public RestUIContext(Resource<?> selection)
+   {
+      super();
+      this.selection = selection;
+   }
 
-	@Override
-	public UIProvider getProvider() {
-		return provider;
-	}
+   @SuppressWarnings("unchecked")
+   @Override
+   public <SELECTIONTYPE> UISelection<SELECTIONTYPE> getInitialSelection()
+   {
+      return (UISelection<SELECTIONTYPE>) Selections.from(selection);
+   }
+
+   @Override
+   public UIProvider getProvider()
+   {
+      return provider;
+   }
 
 }
