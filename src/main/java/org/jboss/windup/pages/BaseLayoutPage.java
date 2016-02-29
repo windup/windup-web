bf505c2ffd0322ddfa67aa43f828e9f1cfd0f904
OndraZizka <zizka@seznam.cz>
2016-02-29 13:51:09 +0100

    Wicket 7.2

--- src/main/java/org/jboss/windup/pages/BaseLayoutPage.java
+++ src/main/java/org/jboss/windup/pages/BaseLayoutPage.java
@@ -1,11 +1,7 @@
 package org.jboss.windup.pages;
 
-import org.apache.wicket.Session;
 import org.apache.wicket.devutils.debugbar.DebugBar;
-import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.request.resource.CssResourceReference;
-import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 
 /**
@@ -19,21 +15,12 @@ public class BaseLayoutPage extends WebPage {
         add( new DebugBar("debugBar") );
     }
 
-
     /**
      *  Global helper to avoid casting everywhere.
     @Override
-    public EsscAuthSession getSession(){
-        return (EsscAuthSession) Session.get();
+    public WindupAuthSession getSession(){
+        return (WindupAuthSession) Session.get();
     }
-     */
+    */
 
-
-    /** Adds CSS reference. */
-    @Override
-    public void renderHead(IHeaderResponse response) {
-        response.renderCSSReference(new CssResourceReference( BaseLayoutPage.class, "default.css" ));
-        response.renderJavaScriptReference(new JavaScriptResourceReference( BaseLayoutPage.class, "common.js" ));
-    }
-
-}// class
+}
--- src/main/java/org/jboss/windup/web/WicketJavaEEApplication.java
+++ src/main/java/org/jboss/windup/web/WicketJavaEEApplication.java
@@ -3,15 +3,15 @@ package org.jboss.windup.web;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import net.ftlines.wicket.cdi.CdiConfiguration;
-import net.ftlines.wicket.cdi.ConversationPropagation;
+import org.apache.wicket.cdi.CdiConfiguration;
+import org.apache.wicket.cdi.ConversationPropagation;
 import org.apache.wicket.*;
 import org.apache.wicket.authorization.Action;
 import org.apache.wicket.authorization.IAuthorizationStrategy;
-import org.apache.wicket.markup.html.IPackageResourceGuard;
-import org.apache.wicket.markup.html.PackageResourceGuard;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.request.component.IRequestableComponent;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.IResource;
 import org.jboss.windup.pages.home.HomePage;
 import org.jboss.windup.pages.statics.AboutPage;
 
@@ -42,7 +42,7 @@ public class WicketJavaEEApplication extends WebApplication {
         }
 
         // Configure CDI, disabling Conversations as we aren't using them
-        new CdiConfiguration(bm).setPropagation(ConversationPropagation.NONE).configure(this);
+        new CdiConfiguration().setFallbackBeanManager(bm).setPropagation(ConversationPropagation.NONE).configure(this);
 
         // Wicket Settings
         // This would prevent Ajax components throwing an exception after session expiration.
@@ -59,44 +59,17 @@ public class WicketJavaEEApplication extends WebApplication {
         //mountPage("/app/${id}", AppPage.class);
         //mountPage("/app/${id}/${run}", RunPage.class);
 
-        initResources();
-
         // Register the authorization strategy
         getSecuritySettings().setAuthorizationStrategy( new WindupAuthStrategy() );
 
     }
 
 
-    /**
-     *  Initialize resources. Mostly registers images as shared resources.
-     */
-    void initResources(){
-
-        IPackageResourceGuard guard =
-        new IPackageResourceGuard() {
-            private final IPackageResourceGuard DEFAULT = new PackageResourceGuard();
-            @Override public boolean accept( Class<?> scope, String path ) {
-                if( path.endsWith(".png") ) return true;
-                if( path.endsWith(".jpg") ) return true;
-                if( path.endsWith(".gif") ) return true;
-                return DEFAULT.accept( scope, path );
-            }
-        };
-        getResourceSettings().setPackageResourceGuard( guard );
-    }
-
-
-
     public static WicketJavaEEApplication getApp(){
         return (WicketJavaEEApplication) Application.get();
     }
 
-    public boolean isDevMode(){
-        return RuntimeConfigurationType.DEVELOPMENT == Application.get().getConfigurationType();
-    }
-
 }
-
 
 
 
@@ -105,16 +78,22 @@ public class WicketJavaEEApplication extends WebApplication {
  *  Authorize instantiation of SecuredPage-marked only for logged users.
  *  @author ondra
  */
-class WindupAuthStrategy implements IAuthorizationStrategy {
-
+class WindupAuthStrategy implements IAuthorizationStrategy
+{
+    @Override
     public boolean isActionAuthorized( Component component, Action action ) {
         // authorize everything
         return true;
     }
 
-
+    @Override
     public <T extends IRequestableComponent> boolean isInstantiationAuthorized( Class<T> componentClass ) {
         return true;
     }
 
+    @Override
+    public boolean isResourceAuthorized(IResource arg0, PageParameters arg1)
+    {
+        return true;
+    }
 }
