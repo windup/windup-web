5865e1dd9d07afe9b93f7891b4da05f09e1fcc72
Jess Sightler <jesse.sightler@gmail.com>
2016-03-02 13:42:50 -0500

    Added version to the furnace-maven-plugin

--- services/src/main/java/org/jboss/windup/web/services/rest/WindupEndpoint.java
+++ services/src/main/java/org/jboss/windup/web/services/rest/WindupEndpoint.java
@@ -1,8 +1,11 @@
 package org.jboss.windup.web.services.rest;
 
 import java.nio.file.Paths;
+import java.util.concurrent.Callable;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -27,32 +30,40 @@ public class WindupEndpoint
     @Inject
     private Furnace furnace;
 
+    @Resource
+    private ManagedExecutorService managedExecutorService;
+
     @POST
-    public void registerApplication(@QueryParam("inputPath") String inputPath) {
+    public void registerApplication(@QueryParam("inputPath") String inputPath)
+    {
 
     }
 
     @GET
     public void executeWindup(@QueryParam("inputPath") String inputPath, @QueryParam("outputPath") String outputPath)
     {
-        Imported<WindupProcessor> importedProcessor = furnace.getAddonRegistry().getServices(WindupProcessor.class);
-        Imported<GraphContextFactory> importedFactory = furnace.getAddonRegistry().getServices(GraphContextFactory.class);
-        WindupProcessor processor = importedProcessor.get();
-        GraphContextFactory factory = importedFactory.get();
+        Runnable windupProcess = () -> {
+            Imported<WindupProcessor> importedProcessor = furnace.getAddonRegistry().getServices(WindupProcessor.class);
+            Imported<GraphContextFactory> importedFactory = furnace.getAddonRegistry().getServices(GraphContextFactory.class);
+            WindupProcessor processor = importedProcessor.get();
+            GraphContextFactory factory = importedFactory.get();
 
-        try (GraphContext context = factory.create(getDefaultPath()))
-        {
-            WindupConfiguration configuration = new WindupConfiguration()
-                        .setGraphContext(context)
-                        .addInputPath(Paths.get(inputPath))
-                        .setOutputDirectory(Paths.get(outputPath));
+            try (GraphContext context = factory.create(getDefaultPath()))
+            {
+                WindupConfiguration configuration = new WindupConfiguration()
+                            .setGraphContext(context)
+                            .setProgressMonitor(progressMonitor)
+                            .addInputPath(Paths.get(inputPath))
+                            .setOutputDirectory(Paths.get(outputPath));
 
-            processor.execute(configuration);
-        }
-        catch (Exception e)
-        {
-            throw new RuntimeException(e);
-        }
+                processor.execute(configuration);
+            }
+            catch (Exception e)
+            {
+                throw new RuntimeException(e);
+            }
+        };
+        managedExecutorService.execute(windupProcess);
     }
 
     private java.nio.file.Path getDefaultPath()
