eebd3ff96f89021ca6e564e91268e952f7eb802e
OndraZizka <zizka@seznam.cz>
2016-03-17 17:07:32 +0100

    Add working links to the reports

--- services/src/test/java/org/jboss/windup/web/services/FurnaceProducerTest.java
+++ services/src/test/java/org/jboss/windup/web/services/FurnaceProducerTest.java
@@ -1,14 +1,8 @@
 package org.jboss.windup.web.services;
 
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.forge.furnace.Furnace;
 import org.jboss.forge.furnace.util.OperatingSystemUtils;
-import org.jboss.shrinkwrap.api.GenericArchive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.windup.graph.GraphContext;
 import org.jboss.windup.graph.GraphContextFactory;
 import org.jboss.windup.graph.model.resource.FileModel;
@@ -16,9 +10,8 @@ import org.jboss.windup.graph.service.FileService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import javax.inject.Inject;
-import java.io.File;
+
 
 @RunWith(Arquillian.class)
 public class FurnaceProducerTest extends AbstractTest
