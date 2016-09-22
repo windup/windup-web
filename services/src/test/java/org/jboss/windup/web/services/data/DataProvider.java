15d0f677c12f7daf90232d8b9cfd75ca7b4e0783
David Klingenberg <dklingen@redhat.com>
2016-09-22 18:35:44 +0200

    Update RegisteredApplicationEndpoint test

---
+++ services/src/test/java/org/jboss/windup/web/services/data/DataProvider.java
@@ -0,0 +1,45 @@
+package org.jboss.windup.web.services.data;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.jboss.windup.web.services.model.ApplicationGroup;
+import org.jboss.windup.web.services.model.MigrationProject;
+import org.jboss.windup.web.services.rest.ApplicationGroupEndpoint;
+import org.jboss.windup.web.services.rest.MigrationProjectEndpoint;
+
+/**
+ * @author <a href="mailto:dklingen@redhat.com">David Klingenberg</a>
+ */
+public class DataProvider
+{
+    private MigrationProjectEndpoint migrationProjectEndpoint;
+    private ApplicationGroupEndpoint applicationGroupEndpoint;
+
+    public DataProvider(ResteasyWebTarget restEasyTarget)
+    {
+        this.migrationProjectEndpoint = restEasyTarget.proxy(MigrationProjectEndpoint.class);
+        this.applicationGroupEndpoint = restEasyTarget.proxy(ApplicationGroupEndpoint.class);
+    }
+
+    public MigrationProject getMigrationProject()
+    {
+        String projectTitle = "Project " + RandomStringUtils.randomAlphabetic(5);
+
+        MigrationProject migrationProject = new MigrationProject();
+        migrationProject.setTitle(projectTitle);
+
+        return this.migrationProjectEndpoint.createMigrationProject(migrationProject);
+    }
+
+    public ApplicationGroup getApplicationGroup(MigrationProject migrationProject)
+    {
+        String groupTitle = "App Group " + RandomStringUtils.randomAlphabetic(5);
+
+        ApplicationGroup applicationGroup = new ApplicationGroup();
+        applicationGroup.setMigrationProject(migrationProject);
+        applicationGroup.setTitle(groupTitle);
+
+        return this.applicationGroupEndpoint.create(applicationGroup);
+    }
+
+}
--- services/src/test/java/org/jboss/windup/web/services/rest/RegisteredApplicationEndpointTest.java
+++ services/src/test/java/org/jboss/windup/web/services/rest/RegisteredApplicationEndpointTest.java
@@ -3,19 +3,32 @@ package org.jboss.windup.web.services.rest;
 import java.beans.BeanInfo;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
-import java.io.File;
+import java.io.*;
+import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.Collection;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
+import org.apache.commons.io.IOUtils;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.arquillian.warp.WarpTest;
+import org.jboss.arquillian.warp.impl.client.transformation.MigratedInspection;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInputImpl;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.jboss.windup.web.services.data.DataProvider;
+import org.jboss.windup.web.services.model.ApplicationGroup;
+import org.jboss.windup.web.services.model.MigrationProject;
 import org.jboss.windup.web.services.model.RegisteredApplication;
 import org.jboss.windup.web.services.AbstractTest;
 import org.junit.Assert;
@@ -23,6 +36,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
+import static org.mockito.Mockito.*;
 
 /**
  * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>
@@ -36,6 +56,8 @@ public class RegisteredApplicationEndpointTest extends AbstractTest
     private URL contextPath;
 
     private RegisteredApplicationEndpoint registeredApplicationEndpoint;
+    private DataProvider dataProvider;
+
 
     @BeforeClass
     public static void setUpClass() throws Exception
@@ -49,45 +71,58 @@ public class RegisteredApplicationEndpointTest extends AbstractTest
     {
         ResteasyClient client = getResteasyClient();
         ResteasyWebTarget target = client.target(contextPath + "rest");
+        this.dataProvider = new DataProvider(target);
 
         this.registeredApplicationEndpoint = target.proxy(RegisteredApplicationEndpoint.class);
     }
 
+    /*
+     * Preconditions: We have a user, project and application group.
+     */
     @Test
     @RunAsClient
     public void testRegisterApp() throws Exception
     {
-        BeanInfo beanInfo = Introspector.getBeanInfo(RegisteredApplication.class);
-        for (PropertyDescriptor propertyDescriptor : beanInfo.getPropertyDescriptors())
-        {
-            System.out.println("Property name: " + propertyDescriptor.getName());
-        }
-
         Collection<RegisteredApplication> existingApps = registeredApplicationEndpoint.getRegisteredApplications();
         Assert.assertEquals(0, existingApps.size());
 
-        File tempFile1 = File.createTempFile(RegisteredApplicationEndpointTest.class.getSimpleName() + ".1", ".ear");
-        File tempFile2 = File.createTempFile(RegisteredApplicationEndpointTest.class.getSimpleName() + ".2", ".ear");
+        String fileContent = "upload test";
 
-        RegisteredApplication dto1 = new RegisteredApplication(tempFile1.getAbsolutePath());
-        RegisteredApplication dto2 = new RegisteredApplication(tempFile2.getAbsolutePath());
-        this.registeredApplicationEndpoint.registerApplication(dto1);
-        this.registeredApplicationEndpoint.registerApplication(dto2);
+        MultipartFormDataInput form  = this.mockMultipartFormData(
+                "a.txt",
+                new ByteArrayInputStream(fileContent.getBytes(StandardCharsets.UTF_8))
+        );
+
+        MigrationProject dummyProject = this.dataProvider.getMigrationProject();
+        ApplicationGroup group = this.dataProvider.getApplicationGroup(dummyProject);
+
+        RegisteredApplication app = this.registeredApplicationEndpoint.registerApplication(form, group.getId());
 
         Collection<RegisteredApplication> apps = registeredApplicationEndpoint.getRegisteredApplications();
-        Assert.assertEquals(2, apps.size());
-        boolean foundPath1 = false;
-        boolean foundPath2 = false;
+        Assert.assertEquals(1, apps.size());
 
-        for (RegisteredApplication app : apps)
-        {
-            if (app.getInputPath().equals(tempFile1.getAbsolutePath()))
-                foundPath1 = true;
-            else if (app.getInputPath().equals(tempFile2.getAbsolutePath()))
-                foundPath2 = true;
-        }
+        File file = new File(app.getInputPath());
+        String readContent = IOUtils.toString(new FileInputStream(file), StandardCharsets.UTF_8);
 
-        Assert.assertTrue(foundPath1);
-        Assert.assertTrue(foundPath2);
+        Assert.assertTrue(file.exists());
+        Assert.assertEquals(fileContent, readContent);
+    }
+
+    private MultipartFormDataInput mockMultipartFormData(String fileName, InputStream stream) throws IOException {
+        MultipartFormDataInput form = mock(MultipartFormDataInput.class);
+        InputPart part =  mock(InputPart.class);
+
+        Map<String, List<InputPart>> paramsMap = new HashMap<>();
+        paramsMap.put("file", Arrays.asList(part));
+
+        when(form.getFormDataMap()).thenReturn(paramsMap);
+
+        MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
+        headers.putSingle("Content-Disposition", "form-data; name=\"file\"; filename=\"" + fileName + "\"");
+
+        when(part.getHeaders()).thenReturn(headers);
+        when(part.getBody(InputStream.class, null)).thenReturn(stream);
+
+        return form;
     }
 }
