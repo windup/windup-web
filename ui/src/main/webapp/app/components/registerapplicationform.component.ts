8bee2d582ebf38632861d0c6f0d4b3b43b556ce2
David Klingenberg <dklingen@redhat.com>
2016-09-22 13:45:31 +0200

    Update application registration process - now uses file upload

--- ui/src/main/webapp/app/components/registerapplicationform.component.ts
+++ ui/src/main/webapp/app/components/registerapplicationform.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from "@angular/core";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
+import {FileUploader} from 'ng2-file-upload/ng2-file-upload';
 
 import {RegisteredApplication} from "windup-services";
 import {RegisteredApplicationService} from "../services/registeredapplication.service";
@@ -9,6 +10,8 @@ import {FileService} from "../services/file.service";
 import {ApplicationGroupService} from "../services/applicationgroup.service";
 import {ApplicationGroup} from "windup-services";
 import {FormComponent} from "./formcomponent.component";
+import {Constants} from "../constants";
+import {KeycloakService} from "../services/keycloak.service";
 
 @Component({
     templateUrl: 'app/components/registerapplicationform.component.html'
@@ -19,6 +22,8 @@ export class RegisterApplicationFormComponent extends FormComponent implements O
     applicationGroup:ApplicationGroup;
     model = <RegisteredApplication>{};
     loading:boolean = true;
+    uploader: FileUploader;
+    multipartUploader: FileUploader;
 
     constructor(
         private _router:Router,
@@ -29,6 +34,8 @@ export class RegisterApplicationFormComponent extends FormComponent implements O
         private _formBuilder: FormBuilder
     ) {
         super();
+        this.uploader = _registeredApplicationService.getUploader();
+        this.multipartUploader = _registeredApplicationService.getMultipartUploader();
     }
 
     ngOnInit():any {
@@ -41,7 +48,16 @@ export class RegisterApplicationFormComponent extends FormComponent implements O
             if (!isNaN(id)) {
                 this.loading = true;
                 this._applicationGroupService.get(id).subscribe(
-                    group => { this.applicationGroup = group; this.loading = false }
+                    group => {
+                        this.applicationGroup = group;
+                        this.loading = false;
+                        this.multipartUploader.setOptions({
+                            url: Constants.REST_BASE + "/registeredApplications/appGroup" + "/" + group.id,
+                            authToken: 'Bearer ' + KeycloakService.auth.authz.token,
+                            disableMultipart: false
+                        });
+
+                    }
                 );
             } else {
                 this.loading = false;
@@ -50,14 +66,18 @@ export class RegisterApplicationFormComponent extends FormComponent implements O
     }
 
     register() {
-        console.log("Registering application: " + this.model.inputPath);
+        if (this.multipartUploader.getNotUploadedItems().length > 0) {
+            console.log("Registering application: " + this.model.inputPath);
 
-        this.model.applicationGroup = this.applicationGroup;
+            this._registeredApplicationService.registerApplication(this.applicationGroup.id).subscribe(
+                application => this.rerouteToApplicationList(),
+                error => this.handleError(<any>error)
+            );
+        } else {
+            this.handleError("Please select file first");
+        }
 
-        this._registeredApplicationService.registerApplication(this.model).subscribe(
-            application => this.rerouteToApplicationList(),
-            error => this.handleError(<any>error)
-        );
+        return false;
     }
 
     rerouteToApplicationList() {
--- ui/src/main/webapp/app/services/registeredapplication.service.ts
+++ ui/src/main/webapp/app/services/registeredapplication.service.ts
@@ -1,31 +1,70 @@
 import {Injectable} from '@angular/core';
 import {Headers, Http, RequestOptions} from '@angular/http';
+import {FileUploader} from 'ng2-file-upload/ng2-file-upload';
+import {Observable} from 'rxjs/Observable';
 
 import {Constants} from "../constants";
 import {RegisteredApplication} from "windup-services";
 import {AbstractService} from "./abtract.service";
+import {KeycloakService} from "./keycloak.service";
 
 @Injectable()
 export class RegisteredApplicationService extends AbstractService {
     private GET_APPLICATIONS_URL = "/registeredApplications/list";
-    private REGISTER_APPLICATION_URL = "/registeredApplications/register";
+    private REGISTER_APPLICATION_URL = "/registeredApplications/appgroup";
+    private UPLOAD_URL = '/file';
+    private _uploader: FileUploader;
+    private _multipartUploader: FileUploader;
 
     constructor (private _http: Http) {
         super();
+        this._uploader = new FileUploader({
+            url: Constants.REST_BASE + this.UPLOAD_URL,
+            authToken: 'Bearer ' + KeycloakService.auth.authz.token,
+            disableMultipart: true
+        });
+
+        this._multipartUploader = new FileUploader({
+            url: Constants.REST_BASE + this.UPLOAD_URL + '/multipart',
+            authToken: 'Bearer ' + KeycloakService.auth.authz.token,
+            disableMultipart: false
+        });
     }
 
-    registerApplication(application:RegisteredApplication) {
-        let headers = new Headers();
-        let options = new RequestOptions({ headers: headers });
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
+    registerApplication(applicationGroupId: number) {
+        let responses = [];
+        let errors = [];
 
-        let body = JSON.stringify(application);
+        let promise = new Promise((resolve, reject) => {
+            this._multipartUploader.onCompleteItem = (item, response, status, headers) => {
+                if (status == 200) {
+                    responses.push(JSON.parse(response));
+                } else {
+                    errors.push(JSON.parse(response));
+                }
+            };
 
-        return this._http.put(Constants.REST_BASE + this.REGISTER_APPLICATION_URL, body, options)
-            .map(res => <RegisteredApplication> res.json())
-            .catch(this.handleError);
+            this._multipartUploader.onCompleteAll = () => {
+                resolve(responses);
+            };
+
+            this._multipartUploader.onErrorItem  = (item, response, status, headers) => {
+                reject(JSON.parse(response));
+            };
+        });
+
+        this._multipartUploader.uploadAll();
+
+        return Observable.fromPromise(promise);
     }
+
+    getUploader() {
+        return this._uploader;
+    }
+
+    getMultipartUploader() {
+        return this._multipartUploader;
+    };
 
     getApplications() {
         let headers = new Headers();
