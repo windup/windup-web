7000bb4c58393ef2c80bac03889d23a72fee257c
David Klingenberg <dklingen@redhat.com>
2016-10-19 17:33:07 +0200

    Update constants

--- ui/src/main/webapp/src/app/constants.ts
+++ ui/src/main/webapp/src/app/constants.ts
@@ -1,7 +1,8 @@
 export class Constants {
-    public static SERVER: string = 'http://localhost:4040';
+    public static SERVER: string = 'http://localhost:8080';
+    public static REST_SERVER: string = 'http://localhost:8082';
     public static SERVER_BASE: string = '/';
-    public static REST_BASE:string = "/windup-web-services/rest";
+    public static REST_BASE: string = Constants.REST_SERVER + "/windup-web-services/rest";
     public static STATIC_REPORTS_BASE:string = "/windup-web-services/staticReport";
     public static UNAUTHENTICATED_PAGE:string = "/not_loggedin.html";
     public static AUTH_REDIRECT_URL: string = Constants.SERVER + '/index.html';
