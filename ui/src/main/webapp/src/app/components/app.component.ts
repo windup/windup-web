6af55a04f9115472050200f82c0bc1f1419492f9
David Klingenberg <dklingen@redhat.com>
2016-11-04 09:25:09 +0100

    Hide navbar when user is not logged in.

--- ui/src/main/webapp/src/app/components/app.component.ts
+++ ui/src/main/webapp/src/app/components/app.component.ts
@@ -1,9 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import '../rxjs-operators';
+import {KeycloakService} from "../services/keycloak.service";
 
 @Component({
     selector: 'windup-app',
     templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    isUserLoggedIn: boolean;
+
+    constructor(private _keycloakService: KeycloakService) {
+
+    }
+
+
+    ngOnInit(): void {
+        this._keycloakService.isLoggedIn().subscribe(isLoggedIn => this.isUserLoggedIn = isLoggedIn);
+    }
 }
