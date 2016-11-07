9595553f7aef6a434755525fa7593216725d2608
Ondrej Zizka <zizka@seznam.cz>
2016-11-07 18:48:21 +0100

    Add a test.

--- ui/src/main/webapp/src/app/constants.ts
+++ ui/src/main/webapp/src/app/constants.ts
@@ -3,6 +3,7 @@ export class Constants {
     public static REST_SERVER: string = 'http://localhost:8080';
     public static SERVER_BASE: string = '/';
     public static REST_BASE: string = Constants.REST_SERVER + "/windup-web-services/rest";
+    public static REST_FURNACE: string = Constants.REST_SERVER + "/windup-web-services/rest-furnace";
     public static STATIC_REPORTS_BASE:string = "/windup-web-services/staticReport";
     public static UNAUTHENTICATED_PAGE:string = "/windup-web/login";
     public static AUTH_REDIRECT_URL: string = Constants.SERVER + '/windup-web/';
