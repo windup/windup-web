c4318e00f86fac2f0fdbb018a4869bf3d868f6e3
David Klingenberg <dklingen@redhat.com>
2016-11-11 15:44:11 +0100

    Add resolvers

---
+++ ui/src/main/webapp/src/app/components/group/application-group.resolve.ts
@@ -0,0 +1,36 @@
+import {Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {ApplicationGroup} from "../../windup-services";
+import {ApplicationGroupService} from "../../services/applicationgroup.service";
+import {NotificationService} from "../../services/notification.service";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class ApplicationGroupResolve implements Resolve<ApplicationGroup> {
+
+    public constructor(
+        private _applicationGroupService: ApplicationGroupService,
+        private _notificationService: NotificationService,
+        private _router: Router
+    ) {
+
+    }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ApplicationGroup|boolean> {
+        let id = +route.params['groupId'];
+
+        return new Observable<ApplicationGroup>(observer => {
+            this._applicationGroupService.get(id).subscribe(
+                applicationGroup => {
+                    observer.next(applicationGroup);
+                    observer.complete();
+                },
+                error => {
+                    this._notificationService.error(error);
+                    this._router.navigate(['/']);
+                    observer.next(false);
+                }
+            );
+        });
+    }
+}
---
+++ ui/src/main/webapp/src/app/services/project.resolve.ts
@@ -0,0 +1,36 @@
+import {Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+import {MigrationProject} from "windup-services";
+import {MigrationProjectService} from "./migrationproject.service";
+import {NotificationService} from "./notification.service";
+
+@Injectable()
+export class ProjectResolve implements Resolve<MigrationProject> {
+
+    public constructor(
+        private _migrationProjectService: MigrationProjectService,
+        private _notificationService: NotificationService,
+        private _router: Router
+    ) {
+
+    }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<MigrationProject|boolean> {
+        let id = +route.params['projectId'];
+
+        return new Observable<MigrationProject>(observer => {
+            this._migrationProjectService.get(id).subscribe(
+                project => {
+                    observer.next(project);
+                    observer.complete();
+                },
+                error => {
+                    this._notificationService.error(error);
+                    this._router.navigate(['/']);
+                    observer.next(false);
+                }
+            );
+        });
+    }
+}
