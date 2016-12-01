39539df682494cd0c350cd1148d93655948cf3d2
David Klingenberg <dklingen@redhat.com>
2016-12-01 15:26:14 +0100

    Report filter frontend.

---
+++ ui/src/main/webapp/src/app/components/reports/filter/report-filter.component.ts
@@ -0,0 +1,85 @@
+import {Component, OnInit, OnDestroy} from "@angular/core";
+import {ApplicationGroup, Tag, ReportFilter} from "windup-services";
+import {ActivatedRoute, Router, NavigationEnd} from "@angular/router";
+import {Subscription} from "rxjs";
+import {ReportFilterService} from "./report-filter.service";
+import {NotificationService} from "../../../services/notification.service";
+import {RouteFlattenerService} from "../../../services/route-flattener.service";
+import {CustomSelectConfiguration} from "../../custom-select/custom-select.component";
+import {RegisteredApplication} from "windup-services";
+import {Category} from "windup-services";
+
+@Component({
+    templateUrl: './report-filter.component.html'
+})
+export class ReportFilterComponent implements OnInit, OnDestroy {
+    group: ApplicationGroup = <ApplicationGroup>{};
+    filter: ReportFilter;
+    tags: Tag[] = [];
+    categories: Category[] = [];
+    routerSubscriptions: Subscription[] = [];
+
+    appSelectConfig: CustomSelectConfiguration;
+    categoryTagSelectConfig: CustomSelectConfiguration;
+
+    constructor(
+        private _router: Router,
+        private _activatedRoute: ActivatedRoute,
+        private _filterService: ReportFilterService,
+        private _notificationService: NotificationService,
+        private _routeFlattenerService: RouteFlattenerService
+    ) {
+        this.filter = this.getDefaultFilter(null);
+
+        this.categoryTagSelectConfig = {
+            getLabel: (tag: Tag) => tag.name,
+            comparator: (a: {id: number}, b: {id: number}) => a.id === b.id
+        };
+
+        this.appSelectConfig = {
+            getLabel: (app: RegisteredApplication) => app.title,
+            comparator: (option: RegisteredApplication, b: RegisteredApplication|number) => {
+                if (typeof b === 'number') {
+                    return option.id === b;
+                } else {
+                    return option.id === b.id;
+                }
+            }
+        };
+    }
+
+    private getDefaultFilter(applicationGroup: ApplicationGroup): ReportFilter {
+        return {
+            id: null,
+            // applicationGroup: applicationGroup,
+            selectedApplications: [],
+            includeTags: [],
+            excludeTags: [],
+            includeCategories: [],
+            excludeCategories: [],
+            enabled: false
+        };
+    }
+
+    ngOnInit(): void {
+        // TODO: For some reason this is being called twice
+        console.log('ReportFilter Init');
+        this.routerSubscriptions.push(this._router.events.filter(event => event instanceof NavigationEnd).subscribe(_ => {
+            let flatData = this._routeFlattenerService.getFlattenedRouteData(this._activatedRoute.snapshot);
+            this.group = flatData.data['applicationGroup'];
+            this.filter = this.group.reportFilter || this.getDefaultFilter(this.group);
+            this._filterService.getTags(this.group).subscribe(tags => this.tags = tags);
+            this._filterService.getCategories(this.group).subscribe(categories => this.categories = categories);
+        }));
+    }
+
+    ngOnDestroy(): void {
+        this.routerSubscriptions.forEach(_ => _.unsubscribe());
+    }
+
+    saveFilter() {
+        this._filterService.updateFilter(this.group, this.filter).subscribe(() => {
+            this._notificationService.success('Filter successfully updated');
+        });
+    }
+}
