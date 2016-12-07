windup/windup-web

commit bed81e04eff9987eee1eed4c457376bb39dd5960

Fixed performance issues and minor cosmetic issues.
 - No longer preload package information along with group lists. Now use a separate service call to get this information for the analysis context form.
    - With larger applications, this can cost megabytes over the wire and result in multisecond delays in the UI
 - No longer display the "Run Windup" option on the context sensitive menu if there are no registered applications.

ui/src/main/webapp/src/app/components/analysis-context-form.component.ts
@@ -12,7 +12,10 @@ import {ModalDialogComponent} from "./modal-dialog.component";
 import {IsDirty} from "../is-dirty.interface";
 import {Observable} from "rxjs/Observable";
 import {PackageRegistryService} from "../services/package-registry.service";
-import {ApplicationGroup, AnalysisContext, Package, MigrationPath, AdvancedOption, RulesPath} from "../windup-services";
+import {
+    ApplicationGroup, AnalysisContext, Package, MigrationPath, AdvancedOption, RulesPath,
+    PackageMetadata
+} from "../windup-services";
 
 @Component({
     templateUrl: 'analysis-context-form.component.html'
@@ -23,7 +26,7 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
     private analysisContextForm:NgForm;
 
     private _dirty: boolean = null;
-
+    loading:boolean = true;
     applicationGroup: ApplicationGroup = null;
 
     analysisContext:AnalysisContext = <AnalysisContext>{};
@@ -57,63 +60,6 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
         this.excludePackages = [];
     }
 
-    ngOnInit() {
-        this._activatedRoute.parent.data.subscribe((data: {applicationGroup: ApplicationGroup}) => {
-            this.applicationGroup = data.applicationGroup;
-
-            this._configurationOptionsService.getAll().subscribe((options:ConfigurationOption[]) => {
-                this.configurationOptions = options;
-            });
-
-            if (this.applicationGroup.packageMetadata.scanStatus === "COMPLETE") {
-                this.applicationGroup.packageMetadata.packageTree.forEach(node => {
-                    this._packageRegistryService.putHierarchy(node);
-                });
-            }
-
-            this.packageTree = this.applicationGroup.packageMetadata.packageTree;
-            this.analysisContext = this.applicationGroup.analysisContext;
-
-            console.log("Loaded analysis context: " + JSON.stringify(this.analysisContext));
-
-            this.initializeAnalysisContext();
-        });
-    }
-
-    initializeAnalysisContext() {
-        if (this.analysisContext == null) {
-            this.analysisContext = <AnalysisContext>{};
-            this.analysisContext.migrationPath = <MigrationPath>{};
-            this.analysisContext.advancedOptions = [];
-            this.analysisContext.includePackages = [];
-            this.analysisContext.excludePackages = [];
-            this.analysisContext.rulesPaths = [];
-        } else {
-            // for migration path, store the id only
-            this.analysisContext.migrationPath = <MigrationPath>{id: this.analysisContext.migrationPath.id};
-            if (this.analysisContext.includePackages == null || this.analysisContext.includePackages.length == 0) {
-                this.includePackages = [];
-            } else {
-                this.includePackages = this.analysisContext.includePackages.map(node => this._packageRegistryService.get(node.id));
-            }
-
-            if (this.analysisContext.excludePackages == null || this.analysisContext.excludePackages.length == 0) {
-                this.analysisContext.excludePackages = [];
-            } else {
-                this.analysisContext.excludePackages = this.analysisContext.excludePackages.map(node => this._packageRegistryService.get(node.id));
-            }
-
-            if (this.analysisContext.rulesPaths == null)
-                this.analysisContext.rulesPaths = [];
-        }
-
-        // Just use the ID here
-        this.analysisContext.applicationGroup = <ApplicationGroup>{id: this.applicationGroup.id};
-
-        this.includePackages = this.analysisContext.includePackages;
-        this.excludePackages = this.analysisContext.excludePackages;
-    }
-
     get migrationPaths() {
         if (this._migrationPathsObservable == null) {
             this._migrationPathsObservable = this._migrationPathService.getAll();
@@ -123,7 +69,6 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
 
     get dirty(): boolean {
         if (this._dirty != null) {
-            console.log("Returning locally set dirty: " + this._dirty);
             return this._dirty;
         }
 
@@ -131,13 +76,92 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
     }
 
     advancedOptionsChanged(advancedOptions:AdvancedOption[]) {
-        console.log("1Advanced options changed... dirty: " + this.dirty);
         this._dirty = true;
-        console.log("2Advanced options changed... dirty: " + this.dirty);
     }
 
     onNodesChanged(event) {
         console.log(event);
+    }
+
+    ngOnInit() {
+        this._activatedRoute.parent.params.subscribe(params => {
+            let id:number = parseInt(params["groupId"]);
+            if (!isNaN(id)) {
+                this.loading = true;
+
+                this._configurationOptionsService.getAll().subscribe(
+                    (options:ConfigurationOption[]) => {
+                        this.configurationOptions = options;
+                    }
+                );
+
+                this._applicationGroupService.get(id).subscribe(
+                    group => {
+                        this.applicationGroup = group;
+
+                        this.loadPackageMetadata();
+
+                        this.analysisContext = group.analysisContext;
+                        console.log("Loaded analysis context: " + JSON.stringify(this.analysisContext));
+
+                        if (this.analysisContext == null) {
+                            this.analysisContext = <AnalysisContext>{};
+                            this.analysisContext.migrationPath = <MigrationPath>{};
+                            this.analysisContext.advancedOptions = [];
+                            this.analysisContext.includePackages = [];
+                            this.analysisContext.excludePackages = [];
+                            this.analysisContext.rulesPaths = [];
+                        } else {
+                            // for migration path, store the id only
+                            this.analysisContext.migrationPath = <MigrationPath>{ id: this.analysisContext.migrationPath.id };
+
+                            if (this.analysisContext.rulesPaths == null)
+                                this.analysisContext.rulesPaths = [];
+                        }
+
+
+                        // Just use the ID here
+                        this.analysisContext.applicationGroup = <ApplicationGroup>{ id: group.id };
+
+                        this.loading = false;
+                    }
+                );
+            } else {
+                this.loading = false;
+                this.errorMessages.push("groupID parameter was not specified!");
+            }
+        });
+    }
+
+    private loadPackageMetadata() {
+        this._applicationGroupService.getPackageMetadata(this.applicationGroup.id).subscribe(
+            (packageMetadata:PackageMetadata) => {
+                if (packageMetadata.scanStatus === "COMPLETE") {
+                    packageMetadata.packageTree.forEach(node => {
+                        this._packageRegistryService.putHierarchy(node);
+                    });
+                }
+
+                this.packageTree = packageMetadata.packageTree;
+
+                if (this.analysisContext != null) {
+                    if (this.analysisContext.includePackages == null || this.analysisContext.includePackages.length == 0) {
+                        this.includePackages = [];
+                    } else {
+                        this.includePackages = this.analysisContext.includePackages.map(node => this._packageRegistryService.get(node.id));
+                    }
+
+                    if (this.analysisContext.excludePackages == null || this.analysisContext.excludePackages.length == 0) {
+                        this.analysisContext.excludePackages = [];
+                    } else {
+                        this.analysisContext.excludePackages = this.analysisContext.excludePackages.map(node => this._packageRegistryService.get(node.id));
+                    }
+                }
+
+                this.includePackages = this.analysisContext.includePackages;
+                this.excludePackages = this.analysisContext.excludePackages;
+            }
+        );
     }
 
     save() {
@@ -176,7 +200,7 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
     }
 
     routeToGroupList() {
-        this._router.navigate(['/projects', this.applicationGroup.migrationProject.id]);
+        this._router.navigate(['/groups', this.applicationGroup.id]);
     }
 }
 

services/src/main/java/org/jboss/windup/web/services/model/ApplicationGroup.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
@@ -64,7 +65,7 @@ public class ApplicationGroup implements Serializable
     @Fetch(FetchMode.SELECT)
     private Set<WindupExecution> executions;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private PackageMetadata packageMetadata;
 
     public ApplicationGroup()
@@ -229,6 +230,7 @@ public class ApplicationGroup implements Serializable
      *
      * @return Package metadata
      */
+    @JsonIgnore
     public PackageMetadata getPackageMetadata()
     {
         return packageMetadata;

services/src/main/java/org/jboss/windup/web/services/model/PackageMetadata.java
@@ -1,6 +1,8 @@
 package org.jboss.windup.web.services.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
@@ -18,6 +20,7 @@ import javax.persistence.*;
  * @author <a href="mailto:dklingenberg@gmail.com">David Klingenberg</a>
  */
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class PackageMetadata implements Serializable
 {
     /**

services/src/main/java/org/jboss/windup/web/services/model/RegisteredApplication.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import org.jboss.windup.web.services.validators.FileExistsConstraint;
 
@@ -56,7 +57,7 @@ public class RegisteredApplication implements Serializable
     @ManyToOne()
     private ApplicationGroup applicationGroup;
 
-    @OneToOne()
+    @OneToOne(fetch = FetchType.LAZY)
     private PackageMetadata packageMetadata;
 
     public RegisteredApplication()
@@ -190,6 +191,7 @@ public class RegisteredApplication implements Serializable
      *
      * @return Package metadata
      */
+    @JsonIgnore
     public PackageMetadata getPackageMetadata()
     {
         return packageMetadata;

services/src/main/java/org/jboss/windup/web/services/rest/ForgeProxyMarshaller.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
+import org.hibernate.proxy.pojo.javassist.JavassistLazyInitializer;
 import org.jboss.forge.furnace.proxy.ClassLoaderAdapterCallback;
 import org.jboss.forge.furnace.proxy.ClassLoaderInterceptor;
 import org.jboss.forge.furnace.proxy.Proxies;
@@ -36,6 +37,7 @@ public class ForgeProxyMarshaller extends JacksonJsonProvider
     {
         ObjectMapper mapper = locateMapper(type, mediaType);
         // Suppress handler properties in JSON output
+        mapper.addMixIn(JavassistLazyInitializer.class, CLACMixin.class);
         mapper.addMixIn(ClassLoaderAdapterCallback.class, CLACMixin.class);
         mapper.addMixIn(ClassLoaderInterceptor.class, CLACMixin.class);
 

services/src/main/java/org/jboss/windup/web/services/service/PackageService.java
@@ -55,6 +55,11 @@ public class PackageService
     @Transactional
     public Collection<Package> discoverPackages(RegisteredApplication application)
     {
+        // Reload it to insure that any lazy loaded fields are still available.
+        // Don't reload if the id is null (can happen in tests or with detached instances)
+        if (application.getId() != null)
+            application = this.entityManager.find(RegisteredApplication.class, application.getId());
+
         PackageMetadata metadata = application.getPackageMetadata();
         metadata.setScanStatus(PackageMetadata.ScanStatus.IN_PROGRESS);
         this.entityManager.merge(metadata);

services/src/test/java/org/jboss/windup/web/services/rest/ApplicationGroupTest.java
@@ -5,7 +5,6 @@ import org.apache.http.HttpStatus;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.dmr.JSONParser;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.windup.web.services.AbstractTest;
@@ -22,11 +21,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.json.stream.JsonParser;
 import javax.ws.rs.core.Response;
 import java.net.URL;
-import javax.json.Json;
-import javax.json.JsonObject;
 
 /**
  * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>

ui/src/main/webapp/src/app/components/layout/group-layout.component.ts
@@ -5,7 +5,7 @@ import {RouteLinkProviderService} from "../../services/route-link-provider-servi
 import {MigrationIssuesComponent} from "../reports/migration-issues/migration-issues.component";
 import {TechnologiesReportComponent} from "../reports/technologies/technologies-report.component";
 import {WindupService} from "../../services/windup.service";
-import {ReportMenuItem} from "../navigation/context-menu-item.class";
+import {ReportMenuItem, ContextMenuItem} from "../navigation/context-menu-item.class";
 import {AnalysisContextFormComponent} from "../analysis-context-form.component";
 import {NotificationService} from "../../services/notification.service";
 import {GroupListComponent} from "../group-list.component";
@@ -62,9 +62,12 @@ export class GroupLayoutComponent implements OnInit {
                 icon: 'fa-cogs',
                 isEnabled: true,
             },
-            {
-                label: 'Run Windup',
-                action: () => {
+            new ContextMenuItem(
+                'Run Windup',
+                'fa-rocket',
+                () => { return this.applicationGroup.applications.length > 0; },
+                null,
+                () => {
                     this._windupService.executeWindupGroup(this.applicationGroup.id).subscribe(
                         success => {
                             this._notificationService.info('Windup execution has started');
@@ -73,10 +76,8 @@ export class GroupLayoutComponent implements OnInit {
                             this._notificationService.error(utils.getErrorMessage(error));
                         }
                     );
-                },
-                icon: 'fa-rocket',
-                isEnabled: true
-            },
+                }
+            ),
             /*
             {
                 label: 'Dashboard',

commit d161a30c509a09dce2f0eb7c8ccb857a14f0333a

Fix analysis context.
It was broken by changes in commit bed81e0  PR #102. It was probably incorrectly rebased.

ui/src/main/webapp/src/app/components/analysis-context-form.component.ts
@@ -12,10 +12,8 @@ import {ModalDialogComponent} from "./modal-dialog.component";
 import {IsDirty} from "../is-dirty.interface";
 import {Observable} from "rxjs/Observable";
 import {PackageRegistryService} from "../services/package-registry.service";
-import {
-    ApplicationGroup, AnalysisContext, Package, MigrationPath, AdvancedOption, RulesPath,
-    PackageMetadata
-} from "../windup-services";
+import {ApplicationGroup, AnalysisContext, Package, MigrationPath, AdvancedOption, RulesPath} from "../windup-services";
+import {PackageMetadata} from "windup-services";
 
 @Component({
     templateUrl: 'analysis-context-form.component.html'
@@ -26,10 +24,10 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
     private analysisContextForm:NgForm;
 
     private _dirty: boolean = null;
-    loading:boolean = true;
+
     applicationGroup: ApplicationGroup = null;
 
-    analysisContext:AnalysisContext = <AnalysisContext>{};
+    analysisContext:AnalysisContext;
     packageTree: Package[] = [];
 
     /**
@@ -55,87 +53,63 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
                 private _configurationOptionsService:ConfigurationOptionsService,
                 private _packageRegistryService: PackageRegistryService) {
         super();
-        this.analysisContext.migrationPath = <MigrationPath>{};
         this.includePackages = [];
         this.excludePackages = [];
     }
 
-    get migrationPaths() {
-        if (this._migrationPathsObservable == null) {
-            this._migrationPathsObservable = this._migrationPathService.getAll();
-        }
-        return this._migrationPathsObservable;
-    }
-
-    get dirty(): boolean {
-        if (this._dirty != null) {
-            return this._dirty;
-        }
-
-        return this.analysisContextForm.dirty;
-    }
-
-    advancedOptionsChanged(advancedOptions:AdvancedOption[]) {
-        this._dirty = true;
-    }
-
-    onNodesChanged(event) {
-        console.log(event);
-    }
-
     ngOnInit() {
-        this._activatedRoute.parent.params.subscribe(params => {
-            let id:number = parseInt(params["groupId"]);
-            if (!isNaN(id)) {
-                this.loading = true;
+        this._activatedRoute.parent.parent.data.subscribe((data: {applicationGroup: ApplicationGroup}) => {
+            this.applicationGroup = data.applicationGroup;
 
-                this._configurationOptionsService.getAll().subscribe(
-                    (options:ConfigurationOption[]) => {
-                        this.configurationOptions = options;
-                    }
-                );
+            this._configurationOptionsService.getAll().subscribe((options:ConfigurationOption[]) => {
+                this.configurationOptions = options;
+            });
 
-                this._applicationGroupService.get(id).subscribe(
-                    group => {
-                        this.applicationGroup = group;
+            this.initializeAnalysisContext();
+            this.loadPackageMetadata();
 
-                        this.loadPackageMetadata();
-
-                        this.analysisContext = group.analysisContext;
-                        console.log("Loaded analysis context: " + JSON.stringify(this.analysisContext));
-
-                        if (this.analysisContext == null) {
-                            this.analysisContext = <AnalysisContext>{};
-                            this.analysisContext.migrationPath = <MigrationPath>{};
-                            this.analysisContext.advancedOptions = [];
-                            this.analysisContext.includePackages = [];
-                            this.analysisContext.excludePackages = [];
-                            this.analysisContext.rulesPaths = [];
-                        } else {
-                            // for migration path, store the id only
-                            this.analysisContext.migrationPath = <MigrationPath>{ id: this.analysisContext.migrationPath.id };
-
-                            if (this.analysisContext.rulesPaths == null)
-                                this.analysisContext.rulesPaths = [];
-                        }
-
-
-                        // Just use the ID here
-                        this.analysisContext.applicationGroup = <ApplicationGroup>{ id: group.id };
-
-                        this.loading = false;
-                    }
-                );
-            } else {
-                this.loading = false;
-                this.errorMessages.push("groupID parameter was not specified!");
-            }
+            console.log("Loaded analysis context: " + JSON.stringify(this.analysisContext));
         });
+    }
+
+    getDefaultAnalysisContext() {
+        let analysisContext = <AnalysisContext>{};
+        analysisContext.migrationPath = <MigrationPath>{id: 0};
+        analysisContext.advancedOptions = [];
+        analysisContext.includePackages = [];
+        analysisContext.excludePackages = [];
+        analysisContext.rulesPaths = [];
+        analysisContext.applicationGroup = <ApplicationGroup>{id: 0};
+
+        return analysisContext;
+    }
+
+    initializeAnalysisContext() {
+        let analysisContext = this.applicationGroup.analysisContext;
+
+        if (analysisContext == null) {
+            analysisContext = this.getDefaultAnalysisContext();
+        } else {
+            // for migration path, store the id only
+            if (analysisContext.migrationPath) {
+                analysisContext.migrationPath = <MigrationPath>{id: this.analysisContext.migrationPath.id};
+            } else {
+                analysisContext.migrationPath = <MigrationPath>{id: 0};
+            }
+
+            if (analysisContext.rulesPaths == null)
+                analysisContext.rulesPaths = [];
+        }
+
+        // Just use the ID here
+        analysisContext.applicationGroup = <ApplicationGroup>{id: this.applicationGroup.id};
+
+        this.analysisContext = analysisContext;
     }
 
     private loadPackageMetadata() {
         this._applicationGroupService.getPackageMetadata(this.applicationGroup.id).subscribe(
-            (packageMetadata:PackageMetadata) => {
+            (packageMetadata: PackageMetadata) => {
                 if (packageMetadata.scanStatus === "COMPLETE") {
                     packageMetadata.packageTree.forEach(node => {
                         this._packageRegistryService.putHierarchy(node);
@@ -162,6 +136,29 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
                 this.excludePackages = this.analysisContext.excludePackages;
             }
         );
+    }
+
+    get migrationPaths() {
+        if (this._migrationPathsObservable == null) {
+            this._migrationPathsObservable = this._migrationPathService.getAll();
+        }
+        return this._migrationPathsObservable;
+    }
+
+    get dirty(): boolean {
+        if (this._dirty != null) {
+            return this._dirty;
+        }
+
+        return this.analysisContextForm.dirty;
+    }
+
+    advancedOptionsChanged(advancedOptions:AdvancedOption[]) {
+        this._dirty = true;
+    }
+
+    onNodesChanged(event) {
+        console.log(event);
     }
 
     save() {
@@ -200,7 +197,7 @@ export class AnalysisContextFormComponent extends FormComponent implements OnIni
     }
 
     routeToGroupList() {
-        this._router.navigate(['/groups', this.applicationGroup.id]);
+        this._router.navigate(['/projects', this.applicationGroup.migrationProject.id]);
     }
 }
 