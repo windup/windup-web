628944a1ae76c185c25f1500e4e00c36e289d689
David Klingenberg <dklingen@redhat.com>
2016-12-07 11:29:10 +0100

    Fix #WINDUP-1249 - editing project.

--- ui/src/main/webapp/src/app/components/migration-project-form.component.ts
+++ ui/src/main/webapp/src/app/components/migration-project-form.component.ts
@@ -1,36 +1,49 @@
-import {Component, OnInit} from "@angular/core";
-import {ActivatedRoute, Router} from "@angular/router";
+import {Component, OnInit, OnDestroy} from "@angular/core";
+import {ActivatedRoute, Router, NavigationEnd} from "@angular/router";
 
 import {MigrationProject} from "../windup-services";
 import {MigrationProjectService} from "../services/migration-project.service";
 import {FormComponent} from "./form.component";
+import {Subscription} from "rxjs";
+import {RouteFlattenerService} from "../services/route-flattener.service";
 
 @Component({
     templateUrl: 'migration-project-form.component.html'
 })
-export class MigrationProjectFormComponent extends FormComponent implements OnInit
+export class MigrationProjectFormComponent extends FormComponent implements OnInit, OnDestroy
 {
+    title: string = 'Create Migration Project';
+
     model:MigrationProject = <MigrationProject>{};
 
     editMode:boolean = false;
 
     errorMessages: string[];
+    private routerSubscription: Subscription;
 
     constructor(
         private _router: Router,
         private _activatedRoute: ActivatedRoute,
-        private _migrationProjectService: MigrationProjectService
+        private _migrationProjectService: MigrationProjectService,
+        private _routeFlattener: RouteFlattenerService
     ) {
         super();
     }
 
-    ngOnInit() {
-        this._activatedRoute.data.subscribe((data: {project: MigrationProject}) => {
-            if (data.project) {
+    ngOnInit(): void {
+        this.routerSubscription = this._router.events.filter(event => event instanceof NavigationEnd).subscribe(_ => {
+            let flatRouteData = this._routeFlattener.getFlattenedRouteData(this._activatedRoute.snapshot);
+            this.title = flatRouteData.data['displayName'];
+
+            if (flatRouteData.data['project']) {
                 this.editMode = true;
-                this.model = data.project;
+                this.model = flatRouteData.data['project'];
             }
         });
+    }
+
+    ngOnDestroy(): void {
+        this.routerSubscription.unsubscribe();
     }
 
     save() {
