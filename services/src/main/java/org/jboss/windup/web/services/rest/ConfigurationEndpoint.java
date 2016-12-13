a8a816b889c99bfb8bdae7a293cde3e82a7eb71d
David Klingenberg <dklingen@redhat.com>
2016-12-13 13:35:31 +0100

    Add endpoint to force configuration reload.

--- services/src/main/java/org/jboss/windup/web/services/rest/ConfigurationEndpoint.java
+++ services/src/main/java/org/jboss/windup/web/services/rest/ConfigurationEndpoint.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -42,4 +43,9 @@ public interface ConfigurationEndpoint
     @GET
     @Path("custom-rulesets")
     Set<RulesPath> getCustomRulesetPaths();
+
+
+    @POST
+    @Path("reload")
+    Configuration reloadConfiguration();
 }
