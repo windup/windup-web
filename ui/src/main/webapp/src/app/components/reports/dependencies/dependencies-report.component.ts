8d14c1d33ed555cc4a930f1344715269ccb78abf
Ondrej Zizka <zizka@seznam.cz>
2016-12-19 07:38:30 +0100

    depsReport WIP

--- ui/src/main/webapp/src/app/components/reports/dependencies/dependencies-report.component.ts
+++ ui/src/main/webapp/src/app/components/reports/dependencies/dependencies-report.component.ts
@@ -1,15 +1,32 @@
-import {Component, OnInit} from "@angular/core";
+import {Observable} from "rxjs";
+import {Component, OnInit, Pipe, PipeTransform} from "@angular/core";
 import {Router, ActivatedRoute, Params} from "@angular/router";
 import {NotificationService} from "../../../services/notification.service";
 import {DependenciesService} from "./dependencies.service";
 import {utils} from '../../../utils';
 
+import {DependenciesReportModel} from "../../../generated/tsModels/DependenciesReportModel";
+import {DependencyReportDependencyGroupModel} from "../../../generated/tsModels/DependencyReportDependencyGroupModel";
 import {ProjectDependencyModel} from "../../../generated/tsModels/ProjectDependencyModel";
 import {ProjectModel} from "../../../generated/tsModels/ProjectModel";
 import {ArchiveModel} from "../../../generated/tsModels/ArchiveModel";
 import {DuplicateArchiveModel} from "../../../generated/tsModels/DuplicateArchiveModel";
 import {WindupConfigurationModel} from "../../../generated/tsModels/WindupConfigurationModel";
+import {ApplicationArchiveModel} from "../../../generated/tsModels/ApplicationArchiveModel";
 import {FileModel} from "../../../generated/tsModels/FileModel";
+
+
+/**
+ * Fetches the given Observable and stores it's result into it's .result property, then calls given continuation.
+ */
+export function fetch <T> (from: Observable<T>, then: (result: T, from: Observable<T> & {result: T}) => any) {
+    if (!from)
+        return console.warn("null passed as Observable."), void 0;
+    from.subscribe(what => {
+        (<any>from).result = what;
+        then(what, <any>from);
+    });
+}
 
 
 @Component({
@@ -19,6 +36,10 @@ import {FileModel} from "../../../generated/tsModels/FileModel";
 export class DependenciesReportComponent implements OnInit
 {
     private execID: number;
+    protected reportModel: DependenciesReportModel;
+    protected archiveGroups: DependencyReportDependencyGroupModel[];
+    /// Not used?
+    protected inputApps: ApplicationArchiveModel[];
     protected rootProjects: ProjectModel[];
     protected dependencies: ProjectDependencyModel[];
 
@@ -33,20 +54,101 @@ export class DependenciesReportComponent implements OnInit
     ngOnInit(): void {
         this._activatedRoute.params.forEach((params: Params) => {
             this.execID = +params['executionId'];
+            this.fetchDepsReportModel();
             this.fetchRootProjects();
+            this.fetchInputApps();
         });
     }
 
-    fetchRootProjects(): void{
+    fetchDepsReportModel(): void {
+        this._depsService.getDepsReportModel(this.execID).subscribe(
+            (depReport: DependenciesReportModel) => {
+                console.debug("windupConfig: ", depReport);
+                this.reportModel = depReport;
+                depReport.projectModel.subscribe(p => (<any>depReport.projectModel).result = p);
+                ///fetch(depReport.projectModel);
+                /// Report -*> ArchiveGroup -*> rootFileModel ~=> ArchiveModel { ~=> DuplicateArchiveModel? .canonicalArchive }
+                // ArchiveModel --> .canonicalProject {
+                //    .rootFileModel.fileName
+                //    .mavenIdentifier
+                //    .name .version ...
+                // }
+
+                // Setting these:
+                // * dependency.canonicalProject.result
+                // * dependency.archiveName
+                depReport.archiveGroups.map(groups => {
+                    this.archiveGroups = groups;
+                    groups.map(dep => {
+                        let dep_:any = dep; // Prevent TS complaints.
+                        //dependency.canonicalProject.subscribe(p => (<any>dependency.canonicalProject).result = p);
+                        fetch(dep.canonicalProject, (depProj) => {
+                            dep_.sha1 = dep.sHA1;
+                            dep_.project = depProj;
+                            fetch(depProj.rootFileModel, (root) => {
+                                dep_.archiveName = root.fileName;
+                            });
+                            dep_.gav = dep_.project.mavenIdentifier;
+                            dep_.sha1url = 'http://search.maven.org/#search|ga|1|1:"' + encodeURL(dep.sHA1, 'ISO-8859-1') + '"';
+                        });
+                    });
+                });
+            },
+            error => {
+                this._notificationService.error(utils.getErrorMessage(error));
+                this._router.navigate(['']);
+            }
+        );
+    }
+
+    fetchInputApps(): void {
+        this._depsService.getWindupConfiguration(this.execID).subscribe(
+            (windupConfig: WindupConfigurationModel) => {
+                console.debug("windupConfig: ", windupConfig);
+                windupConfig.inputPaths.map(app => {
+                });
+            },
+            error => {
+                this._notificationService.error(utils.getErrorMessage(error));
+                this._router.navigate(['']);
+            }
+        );
+    }
+
+    fetchRootProjects(): void {
         this._depsService.getRootProjects(this.execID).subscribe(
             (rootProjects:ProjectModel[]) => {
-                console.log("rootProjects: ", rootProjects);
+                console.debug("rootProjects: ", rootProjects);
                 this.rootProjects = rootProjects;
             },
             error => {
                 this._notificationService.error(utils.getErrorMessage(error));
                 this._router.navigate(['']);
-            });
+            }
+        );
     }
 }
 
+
+
+// TODO
+@Pipe({name: 'encodeURL', pure: false})
+export class EncodeUrlPipe implements PipeTransform {
+   transform(value: any, args: any[] = []) {
+       return value;
+   }
+}
+
+@Pipe({name: 'sortDependencyGroupArchivesByPath', pure: false})
+export class SortDependencyGroupArchivesByPathPipe implements PipeTransform {
+   transform(value: any, args: any[] = []) {
+       return value;
+   }
+}
+
+@Pipe({name: 'sortDependencyArchivesByPath', pure: false})
+export class sortDependencyArchivesByPathAscendingPipe implements PipeTransform {
+   transform(value: any, args: any[] = []) {
+       return value;
+   }
+}
--- ui/src/main/webapp/src/app/components/reports/dependencies/dependencies.service.ts
+++ ui/src/main/webapp/src/app/components/reports/dependencies/dependencies.service.ts
@@ -2,12 +2,17 @@ import {Injectable} from "@angular/core";
 import {Http, Response} from "@angular/http";
 import {Observable} from "rxjs";
 import {AbstractService} from "../../../services/abtract.service";
+import {BaseModel} from '../../../../app/services/graph/base.model';
 import {ProjectDependencyModel} from "../../../generated/tsModels/ProjectDependencyModel";
+import {WindupConfigurationModel} from "../../../generated/tsModels/WindupConfigurationModel";
+import {DependenciesReportModel} from "../../../generated/tsModels/DependenciesReportModel";
 import {ProjectModel} from "../../../generated/tsModels/ProjectModel";
 import {Constants} from "../../../constants";
 
 // Windup
 import {GraphJSONToModelService} from '../../../../app/services/graph/graph-json-to-model.service';
+
+let SHARED_LIBS_UNIQUE_ID = "<shared-libs>";
 
 @Injectable()
 export class DependenciesService extends AbstractService
@@ -17,6 +22,60 @@ export class DependenciesService extends AbstractService
     constructor(
         private _http: Http
     ) { super(); }
+
+    /**
+     * Returns given models from the graph.
+     */
+    getModel <T extends BaseModel> (executionId: number, clazz: typeof BaseModel): Observable<T[]> {
+        let service = new GraphJSONToModelService();
+        let url = `${Constants.GRAPH_REST_BASE}/graph/${executionId}/by-type/` + (clazz).name + "?depth=1";
+        return this._http.get(url)
+            .map((res:Response) => res.json())
+            .map((data:any) => {
+                if (!Array.isArray(data) || data.length == 0) {
+                    throw new Error("No items returned, URL: " + url);
+                }
+                let models = service.fromJSONarray(data, this._http);
+                return Observable.of(models);
+            })
+            .catch(this.handleError);
+    }
+
+    /**
+     * Returns the DependenciesReportModel
+     */
+    getDepsReportModel(executionId: number): Observable<DependenciesReportModel> {
+        let service = new GraphJSONToModelService();
+        let url = `${Constants.GRAPH_REST_BASE}/graph/${executionId}/by-type/` + DependenciesReportModel.name + "?depth=1";
+        return this._http.get(url)
+            .map((res:Response) => res.json())
+            .map((data:any) => {
+                if (!Array.isArray(data) || data.length == 0) {
+                    throw new Error("No items returned, URL: " + url);
+                }
+                let reportModel = <DependenciesReportModel>service.fromJSONarray(data, this._http)[0];
+                return Observable.of(reportModel);
+            })
+            .catch(this.handleError);
+    }
+
+    /**
+     * Returns the WindupConfigurationModel
+     */
+    getWindupConfiguration(executionId: number): Observable<WindupConfigurationModel> {
+        let service = new GraphJSONToModelService();
+        let url = `${Constants.GRAPH_REST_BASE}/graph/${executionId}/by-type/` + WindupConfigurationModel.name + "?depth=1";
+        return this._http.get(url)
+            .map((res:Response) => res.json())
+            .map((data:any) => {
+                if (!Array.isArray(data) || data.length == 0) {
+                    throw new Error("No items returned, URL: " + url);
+                }
+                let windupConfig = <WindupConfigurationModel>service.fromJSONarray(data, this._http)[0];
+                return Observable.of(windupConfig);
+            })
+            .catch(this.handleError);
+    }
 
     /**
      * Returns an array of root projects.
@@ -30,7 +89,7 @@ export class DependenciesService extends AbstractService
         return this._http.get(url)
             .map((res:Response) => res.json())
             .map((data:any) => {
-                console.log("Data items: ", data);///
+                console.debug("Data items: ", data);
                 if (!Array.isArray(data) || data.length == 0) {
                     throw new Error("No items returned, URL: " + url);
                 }
