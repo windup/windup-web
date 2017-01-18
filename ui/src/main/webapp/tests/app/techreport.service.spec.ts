551411041bf1c6b9720c0b9c7eb6e18ae0ae71e9
David Klingenberg <dklingen@redhat.com>
2017-01-18 17:13:10 +0100

    Tech techreport service test.

--- ui/src/main/webapp/tests/app/techreport.service.spec.ts
+++ ui/src/main/webapp/tests/app/techreport.service.spec.ts
@@ -2,18 +2,16 @@ import {
     HttpModule, RequestMethod, ResponseOptions, Response, BaseRequestOptions, Http,
     ConnectionBackend
 } from '@angular/http';
-
+import {MockBackend, MockConnection} from "@angular/http/testing";
 import {TestBed, async, inject} from '@angular/core/testing';
 
 import 'rxjs/Rx';
 
 import {Constants} from '../../src/app/constants';
 import {TechReportService} from "../../src/app/components/reports/technologies/tech-report.service";
-import {MockBackend, MockConnection} from "@angular/http/testing";
 import {TECH_REPORT_DATA} from "./techreport-data";
 
-
-describe("Registered Tech Report Service Test", () => {
+describe("TechReportService Test", () => {
     beforeEach(() => {
         TestBed.configureTestingModule(
             {
@@ -37,7 +35,9 @@ describe("Registered Tech Report Service Test", () => {
         (service: TechReportService, mockBackend: MockBackend) => {
 
             mockBackend.connections.subscribe((connection: MockConnection) => {
-                expect(connection.request.url).toEqual(Constants.GRAPH_REST_BASE + '/graph/14/by-type/TechnologiesStats?depth=1');
+                expect(connection.request.url).toEqual(
+                    Constants.GRAPH_REST_BASE + '/graph/14/by-type/ProjectTechnologiesStats?depth=2&includeInVertices=false'
+                );
                 expect(connection.request.method).toEqual(RequestMethod.Get);
 
                 connection.mockRespond(new Response(new ResponseOptions({
@@ -47,6 +47,9 @@ describe("Registered Tech Report Service Test", () => {
 
             service.getStats(14).toPromise()
                 .then(techStat => {
+                    techStat[0].projectModel.subscribe(projectModel => expect(projectModel.name).toEqual("Description Web Services"));
+                    techStat[1].projectModel.subscribe(projectModel => expect(projectModel.name).toEqual("Description Web Services"));
+                    techStat[2].projectModel.subscribe(projectModel => expect(projectModel.name).toEqual("Archives shared by multiple applications"));
                 }, error => { fail(error); });
         }))
     );
