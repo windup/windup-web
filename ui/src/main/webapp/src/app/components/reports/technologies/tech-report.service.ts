de826241d87287f14d99f11800312e12d0cf8a6f
David Klingenberg <dklingen@redhat.com>
2017-01-18 17:54:59 +0100

    Use graph service in tech-report.

--- ui/src/main/webapp/src/app/components/reports/technologies/tech-report.service.ts
+++ ui/src/main/webapp/src/app/components/reports/technologies/tech-report.service.ts
@@ -1,44 +1,23 @@
 import {Injectable} from "@angular/core";
-import {Http, Response} from "@angular/http";
-import {Constants} from "../../../constants";
-import {AbstractService} from "../../../services/abtract.service";
+import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 
-// Windup
-import {GraphJSONToModelService} from "../../../services/graph/graph-json-to-model.service";
-
-// Models
-import {TechnologiesStatsModel} from "../../../generated/tsModels/TechnologiesStatsModel";
 import {ProjectTechnologiesStatsModel} from "../../../generated/tsModels/ProjectTechnologiesStatsModel";
-
+import {GraphService} from "../../../services/graph.service";
 
 @Injectable()
-export class TechReportService extends AbstractService
+export class TechReportService extends GraphService
 {
-    constructor(
-        private http: Http
-    ) { super(); }
-
-    static WINDUP_REST_URL = Constants.REST_SERVER + "/windup-web-services/rest-furnace"; //"http://localhost:8080/
-    static DISCR_TECH_STATS =    ProjectTechnologiesStatsModel.discriminator; //"TechnologiesStats";
-    static INVOKER_URL =         `${TechReportService.WINDUP_REST_URL}/technologyStats/create?exec=`;
-    static GRAPH_TECHSTATS_URL = `${TechReportService.WINDUP_REST_URL}/graph/#{execID}/by-type/${TechReportService.DISCR_TECH_STATS}?depth=2&includeInVertices=false`;
-
+    constructor(http: Http) {
+        super(http);
+    }
 
     getStats(execID: number): Observable<ProjectTechnologiesStatsModel[]>
     {
-        let service = new GraphJSONToModelService();
-        let url = TechReportService.GRAPH_TECHSTATS_URL.replace(/#\{execID\}/, ""+execID);
-        return this.http.get(url)
-            .map((res:Response) => (res.json()))
-            .map((data:any) => {
-                if (!Array.isArray(data) || data.length == 0) {
-                    throw new Error("No items returned, URL: " + url);
-                }
-
-                return service.fromJSONarray(data, this.http);
-            })
-            .catch( (error, caught) => Observable.throw(error));
+        return this.getTypeAsArray<ProjectTechnologiesStatsModel>(ProjectTechnologiesStatsModel.discriminator, execID, {
+            depth: 2,
+            includeInVertices: false
+        });
     }
  }
 
