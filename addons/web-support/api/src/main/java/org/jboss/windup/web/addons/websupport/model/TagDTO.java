a69160488d9b5c5701588fa263c4f16d8de571cb
Jess Sightler <jesse.sightler@gmail.com>
2017-01-30 12:02:08 -0500

    Fixed a serialization issue

--- addons/web-support/api/src/main/java/org/jboss/windup/web/addons/websupport/model/TagDTO.java
+++ addons/web-support/api/src/main/java/org/jboss/windup/web/addons/websupport/model/TagDTO.java
@@ -1,10 +1,14 @@
 package org.jboss.windup.web.addons.websupport.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
 /**
  * @author <a href="mailto:dklingenberg@gmail.com">David Klingenberg</a>
  */
+@JsonIgnoreProperties("handler")
 public interface TagDTO
 {
     String getTagName();
