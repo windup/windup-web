3ab1f5fc5aece9b87af3d4136204343e44a204a1
David Klingenberg <dklingen@redhat.com>
2017-02-15 09:59:12 +0100

    Add tests for project list endpoint.

--- services/src/test/java/org/jboss/windup/web/services/rest/MigrationProjectEndpointTest.java
+++ services/src/test/java/org/jboss/windup/web/services/rest/MigrationProjectEndpointTest.java
@@ -1,5 +1,9 @@
 package org.jboss.windup.web.services.rest;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collection;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -8,15 +12,13 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.windup.web.services.AbstractTest;
 import org.jboss.windup.web.services.ServiceTestUtil;
+import org.jboss.windup.web.services.data.DataProvider;
 import org.jboss.windup.web.services.data.ServiceConstants;
 import org.jboss.windup.web.services.model.MigrationProject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.net.URL;
-import java.util.Collection;
 
 /**
  * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>
@@ -28,6 +30,7 @@ public class MigrationProjectEndpointTest extends AbstractTest
     private URL contextPath;
 
     private MigrationProjectEndpoint migrationProjectEndpoint;
+    private DataProvider dataProvider;
 
     @Before
     public void setUp()
@@ -36,6 +39,7 @@ public class MigrationProjectEndpointTest extends AbstractTest
         ResteasyWebTarget target = client.target(contextPath + ServiceConstants.REST_BASE);
 
         this.migrationProjectEndpoint = target.proxy(MigrationProjectEndpoint.class);
+        this.dataProvider = new DataProvider(target);
     }
 
     @Test
@@ -50,11 +54,59 @@ public class MigrationProjectEndpointTest extends AbstractTest
         MigrationProject migrationProject = new MigrationProject();
         migrationProject.setTitle(title);
 
-        this.migrationProjectEndpoint.createMigrationProject(migrationProject);
+        MigrationProject createdProject = this.migrationProjectEndpoint.createMigrationProject(migrationProject);
 
         Collection<MigrationProjectEndpoint.MigrationProjectAndAppCount> apps = migrationProjectEndpoint.getMigrationProjects();
         Assert.assertEquals(1, apps.size());
         Assert.assertNotNull(apps.iterator().next());
         Assert.assertEquals(title, apps.iterator().next().getMigrationProject().getTitle());
+
+        this.migrationProjectEndpoint.deleteProject(createdProject);
+    }
+
+    @Test
+    @RunAsClient
+    public void testGetProjectListOneProjectWithoutApps()
+    {
+        MigrationProject project = this.dataProvider.getMigrationProject();
+
+        try
+        {
+            Collection<MigrationProjectEndpoint.MigrationProjectAndAppCount> existingProjects = migrationProjectEndpoint.getMigrationProjects();
+            Assert.assertEquals(1, existingProjects.size());
+
+            MigrationProjectEndpoint.MigrationProjectAndAppCount projectFromServer = existingProjects.iterator().next();
+
+            Assert.assertEquals(0L, (long) projectFromServer.applicationCount);
+            Assert.assertEquals(project.getTitle(), projectFromServer.getMigrationProject().getTitle());
+        }
+        finally
+        {
+            this.migrationProjectEndpoint.deleteProject(project);
+        }
+    }
+
+    @Test
+    @RunAsClient
+    public void testGetProjectListOneProjectWithApps() throws IOException
+    {
+        MigrationProject project = this.dataProvider.getMigrationProject();
+        this.dataProvider.getApplication(project);
+        this.dataProvider.getApplication(project);
+
+        try
+        {
+            Collection<MigrationProjectEndpoint.MigrationProjectAndAppCount> existingProjects = migrationProjectEndpoint.getMigrationProjects();
+            Assert.assertEquals(1, existingProjects.size());
+
+            MigrationProjectEndpoint.MigrationProjectAndAppCount projectFromServer = existingProjects.iterator().next();
+
+            Assert.assertEquals(2L, (long) projectFromServer.applicationCount);
+            Assert.assertEquals(project.getTitle(), projectFromServer.getMigrationProject().getTitle());
+        }
+        finally
+        {
+            this.migrationProjectEndpoint.deleteProject(project);
+        }
     }
 }
