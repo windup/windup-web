62cb0c6ac881b1b623759d0db2334d85ad4dee3a
David Klingenberg <dklingen@redhat.com>
2017-03-03 11:03:23 +0100

    Update navbar - change link isActive method.
    Navbar is being redrawn on certain pages. I'm not sure about the reason. Angular change detector run isLinkActive method many times and completely unnecessarily. I changed that to use Angular way of detecting if link is active. It seems to reduce redrawing at least for certain pages.

--- ui/src/main/webapp/src/app/shared/navigation/navbar.component.ts
+++ ui/src/main/webapp/src/app/shared/navigation/navbar.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit, ElementRef} from '@angular/core';
-import {Router} from "@angular/router";
 import {KeycloakService} from "../../core/authentication/keycloak.service";
 import * as $ from 'jquery';
 import 'bootstrap';
@@ -9,7 +8,7 @@ import 'bootstrap';
     templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-    constructor(private _keycloak:KeycloakService, private _router:Router, private _element: ElementRef) {
+    constructor(private _keycloak: KeycloakService, private _element: ElementRef) {
 
     }
 
@@ -17,23 +16,12 @@ export class NavbarComponent implements OnInit {
         $(this._element.nativeElement).find('.dropdown-toggle').dropdown();
     }
 
-    get username():String {
+    get username(): String {
         return this._keycloak.username;
     }
 
     logout(event:Event):void {
         event.preventDefault();
         this._keycloak.logout();
-    }
-
-    isActive(link:HTMLAnchorElement):boolean {
-        if (link == null)
-            return false;
-
-        let linkAttribute = link.attributes.getNamedItem("routerLink");
-        if (linkAttribute == null)
-            return false;
-
-        return linkAttribute.value == this._router.url;
     }
 }
