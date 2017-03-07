c3aea0c53edc0e8023178f4f242b2a098b8f77b4
David Klingenberg <dklingen@redhat.com>
2017-03-07 13:38:29 +0100

    Remove multiple apps test for package discovery
    There is no concatenated output for package discovery now.
    Frontend will get packages from each application and merge it.
    
    Update package service

--- services/src/test/java/org/jboss/windup/web/services/service/PackageServiceTest.java
+++ services/src/test/java/org/jboss/windup/web/services/service/PackageServiceTest.java
@@ -11,7 +11,6 @@ import javax.persistence.EntityManager;
 import org.jboss.windup.web.addons.websupport.services.PackageDiscoveryService;
 import org.jboss.windup.web.furnaceserviceprovider.WebProperties;
 import org.jboss.windup.web.services.data.DataProvider;
-import org.jboss.windup.web.services.model.ApplicationGroup;
 import org.jboss.windup.web.services.model.Package;
 import org.jboss.windup.web.services.model.PackageMetadata;
 import org.jboss.windup.web.services.model.RegisteredApplication;
@@ -128,13 +127,7 @@ public class PackageServiceTest
         PackageMetadataMock appMetadataMock = new PackageMetadataMock();
         RegisteredApplication application = this.getApplication(appMetadataMock);
 
-        ApplicationGroup applicationGroup = new ApplicationGroup();
-        PackageMetadataMock groupMetadataMock = new PackageMetadataMock();
-        applicationGroup.setPackageMetadata(groupMetadataMock);
-        applicationGroup.addApplication(application);
-
         Assert.assertEquals(PackageMetadata.ScanStatus.QUEUED, appMetadataMock.getScanStatus());
-        Assert.assertEquals(PackageMetadata.ScanStatus.QUEUED, groupMetadataMock.getScanStatus());
 
         Collection<Package> discoveredPackages = this.packageService.discoverPackages(application);
 
@@ -143,97 +136,7 @@ public class PackageServiceTest
         // Track changes of status - first it should be set to IN_PROGRESS and then to COMPLETE
         Assert.assertEquals(PackageMetadata.ScanStatus.IN_PROGRESS, appMetadataMock.scanStatusList.get(0));
         Assert.assertEquals(PackageMetadata.ScanStatus.COMPLETE, appMetadataMock.scanStatusList.get(1));
-
         Assert.assertEquals(PackageMetadata.ScanStatus.COMPLETE, appMetadataMock.getScanStatus());
-        Assert.assertEquals(PackageMetadata.ScanStatus.COMPLETE, groupMetadataMock.getScanStatus());
-    }
-
-    /**
-     * Tests only status changes of PackageMetadata for RegisteredApplications and ApplicationGroup. ApplicationGroup has 2 applications, only 1 is
-     * scanned.
-     */
-    @Test
-    public void testDiscoverPackagesMultipleAppsPartialStatusChanges()
-    {
-        RegisteredApplication firstApplication = this.getApplication(new PackageMetadataMock());
-        RegisteredApplication secondApplication = this.getApplication(new PackageMetadataMock());
-
-        ApplicationGroup applicationGroup = new ApplicationGroup();
-        PackageMetadataMock groupMetadataMock = new PackageMetadataMock();
-        applicationGroup.setPackageMetadata(groupMetadataMock);
-        applicationGroup.addApplication(firstApplication);
-        applicationGroup.addApplication(secondApplication);
-
-        Assert.assertEquals(PackageMetadata.ScanStatus.QUEUED, groupMetadataMock.getScanStatus());
-
-        this.packageService.discoverPackages(firstApplication);
-
-        Assert.assertEquals(PackageMetadata.ScanStatus.COMPLETE, firstApplication.getPackageMetadata().getScanStatus());
-        Assert.assertEquals(PackageMetadata.ScanStatus.QUEUED, secondApplication.getPackageMetadata().getScanStatus());
-
-        Assert.assertEquals(PackageMetadata.ScanStatus.IN_PROGRESS, groupMetadataMock.getScanStatus());
-    }
-
-    /**
-     * Tests status changes of PackageMetadata for RegisteredApplications and ApplicationGroup.
-     *
-     * ApplicationGroup has 2 applications, both are scanned.
-     */
-    @Test
-    public void testDiscoverPackagesMultipleApps()
-    {
-        RegisteredApplication firstApplication = this.getApplication(new PackageMetadataMock());
-        RegisteredApplication secondApplication = this.getApplication(new PackageMetadataMock());
-
-        ApplicationGroup applicationGroup = new ApplicationGroup();
-        PackageMetadataMock groupMetadataMock = new PackageMetadataMock();
-        applicationGroup.setPackageMetadata(groupMetadataMock);
-        applicationGroup.addApplication(firstApplication);
-        applicationGroup.addApplication(secondApplication);
-
-        // First test status changes
-        Assert.assertEquals(PackageMetadata.ScanStatus.QUEUED, groupMetadataMock.getScanStatus());
-
-        this.packageService.discoverPackages(firstApplication);
-        this.packageService.discoverPackages(secondApplication);
-
-        Assert.assertEquals(PackageMetadata.ScanStatus.COMPLETE, firstApplication.getPackageMetadata().getScanStatus());
-        Assert.assertEquals(PackageMetadata.ScanStatus.COMPLETE, secondApplication.getPackageMetadata().getScanStatus());
-
-        Assert.assertEquals(PackageMetadata.ScanStatus.COMPLETE, groupMetadataMock.getScanStatus());
-        Assert.assertEquals(PackageMetadata.ScanStatus.IN_PROGRESS, groupMetadataMock.scanStatusList.get(0));
-        Assert.assertEquals(PackageMetadata.ScanStatus.IN_PROGRESS, groupMetadataMock.scanStatusList.get(1));
-        Assert.assertEquals(PackageMetadata.ScanStatus.IN_PROGRESS, groupMetadataMock.scanStatusList.get(2));
-        Assert.assertEquals(PackageMetadata.ScanStatus.COMPLETE, groupMetadataMock.scanStatusList.get(3));
-
-        // Then test packages added to application group
-        ArrayList<Package> resultList = new ArrayList<>(groupMetadataMock.getPackages());
-        Collections.sort(resultList, (Package a, Package b) -> a.getFullName().compareTo(b.getFullName()));
-
-        // There shouldn't be any duplicities
-        Collection<Package> duplicities = this.getDuplicatePackages(resultList);
-        Assert.assertEquals(0, duplicities.size());
-    }
-
-    protected Collection<Package> getDuplicatePackages(Collection<Package> discoveredPackages)
-    {
-        final List<Package> duplicatedPackages = new ArrayList<Package>();
-        Map<String, Package> map = new HashMap<String, Package>()
-        {
-            @Override
-            public Package put(String key, Package value)
-            {
-                if (this.containsKey(key))
-                {
-                    duplicatedPackages.add(value);
-                }
-                return super.put(key, value);
-            }
-        };
-
-        discoveredPackages.forEach(aPackage -> map.put(aPackage.getFullName(), aPackage));
-
-        return duplicatedPackages;
     }
 
     protected RegisteredApplication getApplication(PackageMetadata packageMetadata)
