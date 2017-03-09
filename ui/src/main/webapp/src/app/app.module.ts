ecbb9eaaee98ab605317acea4eeb68937b51c676
David Klingenberg <dklingen@redhat.com>
2017-03-09 13:26:11 +0100

    Create ContextMenuLink component
    Currently items in ContextMenu don't have any real link, they have click
    action which triggers either routing or some action.
    Having actual link on items is more user friendly, since it provides
    option to use 'wheel click'.
    
    User can click with mouse wheel or use ctrl-click and open link in new
    tab. Also link can be copied.
    
    ContextMenuLink component will handle click actions when defined, or
    provide [routerLink] with URL, if item has some.
    
    Due to some angular limitations, (there is no replace: true style for
     components) it needs to be used as attribute selector on parent
     element for current link.

--- ui/src/main/webapp/src/app/app.module.ts
+++ ui/src/main/webapp/src/app/app.module.ts
@@ -117,6 +117,7 @@ import {WizardLayoutComponent} from "./shared/layout/wizard-layout.component";
 import {ExecutionsLayoutComponent} from "./executions/executions-layout.component";
 import {NavbarSelectionComponent} from "./shared/navigation/navbar-selection.component";
 import {DatePipe} from "@angular/common";
+import {ContextMenuLinkComponent} from "./shared/navigation/context-menu-link.component";
 
 /**
  * Load all mapping data from the generated files.
@@ -218,7 +219,8 @@ initializeModelMappingData();
         StatusIconComponent,
         ApplicationListComponent,
         ProjectExecutionsComponent,
-        NavbarSelectionComponent
+        NavbarSelectionComponent,
+        ContextMenuLinkComponent
     ],
     providers: [
         appRoutingProviders,
