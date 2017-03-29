b51c06c88d966c525f2b5e85b0aa29f4ae33471f
Jess Sightler <jesse.sightler@gmail.com>
2017-03-29 16:03:17 -0400

    Added tags and point information to the application list.

--- ui/src/main/webapp/src/app/reports/execution-application-list/execution-application-list.component.ts
+++ ui/src/main/webapp/src/app/reports/execution-application-list/execution-application-list.component.ts
@@ -1,12 +1,17 @@
 import {Component, OnDestroy, OnInit} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 
-import {FilterApplication, WindupExecution} from "windup-services";
+import {FilterApplication, ProjectTraversalReducedDTO, WindupExecution} from "windup-services";
 import {NotificationService} from "../../core/notification/notification.service";
 import {RoutedComponent} from "../../shared/routed.component";
 import {RouteFlattenerService} from "../../core/routing/route-flattener.service";
 import {WindupService} from "../../services/windup.service";
 import {OrderDirection} from "../../shared/sort/sorting.service";
+import {
+    ApplicationDetailsFullDTO, ApplicationDetailsService,
+    ProjectTraversalFullDTO, TagFullDTO
+} from "../application-details/application-details.service";
+import {utils} from "../../shared/utils";
 
 @Component({
     templateUrl: './execution-application-list.component.html',
@@ -25,12 +30,17 @@ export class ExecutionApplicationListComponent extends RoutedComponent implement
     sortedApplications: FilterApplication[] = [];
     searchText: string;
 
+    applicationDetailsDTO:ApplicationDetailsFullDTO;
+    pointsByApplication:Map<number, number> = new Map<number, number>();
+    tagsByApplication:Map<number, {name: string, level: string}[]> = new Map<number, {name: string, level: string}[]>();
+
     constructor(
         _activatedRoute: ActivatedRoute,
         _routeFlattener: RouteFlattenerService,
         _router: Router,
         private _windupService: WindupService,
         private _notificationService: NotificationService,
+        private _applicationDetailsService:ApplicationDetailsService,
     ) {
         super(_router, _activatedRoute, _routeFlattener);
     }
@@ -53,6 +63,14 @@ export class ExecutionApplicationListComponent extends RoutedComponent implement
         this.filteredApplications = execution.filterApplications;
         this.sortedApplications = execution.filterApplications;
         this.updateSearch();
+
+        this._applicationDetailsService.getApplicationDetailsData(this.execID).subscribe(
+            applicationDetailsDto => {
+                this.applicationDetailsDTO = applicationDetailsDto;
+                this.flattenReportData();
+            },
+            error => this._notificationService.error(utils.getErrorMessage(error))
+        );
     }
 
     ngOnDestroy() {
@@ -64,5 +82,78 @@ export class ExecutionApplicationListComponent extends RoutedComponent implement
         } else {
             this.filteredApplications = this.execution.filterApplications;
         }
+    }
+
+    private flattenReportData() {
+        this.applicationDetailsDTO.traversals.forEach(traversal => {
+            // 1. Get the Filter Application
+            let filterApplication = this.getFilterApplication(traversal);
+            console.log("Found app: ", filterApplication);
+
+            if (!filterApplication)
+                return;
+
+            this.pointsByApplication.set(filterApplication.id, 0);
+            this.tagsByApplication.set(filterApplication.id, []);
+            // 2. Traverse and add up the points
+            this.flattenReportDataForTraversal(filterApplication, traversal);
+        });
+
+        this.tagsByApplication.forEach((value, index, map) => {
+            this.tagsByApplication.set(index, value.sort((a, b) => {
+                if (a.level != b.level) {
+                    if (a.level == "IMPORTANT")
+                        return -1;
+                    else if (b.level == "IMPORTANT")
+                        return 1;
+
+                    // These should be the only two cases, but if for some reason there is an unknown tag,
+                    // just use name comparisong below.
+                }
+
+                return a.name.localeCompare(b.name);
+            }));
+        });
+    }
+
+    private flattenReportDataForTraversal(filterApplication:FilterApplication, traversal:ProjectTraversalFullDTO) {
+        let addPointsToMap = (points:number) => {
+            let existingPoints = this.pointsByApplication.get(filterApplication.id);
+            if (!existingPoints)
+                this.pointsByApplication.set(filterApplication.id, points);
+            else
+                this.pointsByApplication.set(filterApplication.id, existingPoints + points);
+        };
+
+        let addTag = (tag:TagFullDTO) => {
+            // TODO/FIXME: This is a hack (same hack as in windup core - in application_list.ftl)
+            if (tag.nameString == "Decompiled Java File")
+                return;
+
+            let existingTags = this.tagsByApplication.get(filterApplication.id);
+            if (existingTags.find(existingTag => existingTag.name == tag.nameString))
+                return;
+
+            existingTags.push({name: tag.nameString, level: tag.levelString});
+        };
+
+        // Just make sure that we always at least zero it out.
+        addPointsToMap(0);
+
+        traversal.files.forEach(file => {
+            file.tags.forEach(tag => addTag(tag));
+
+            file.hints.forEach(hint => addPointsToMap(hint.effort));
+            file.classifications.forEach(classification => addPointsToMap(classification.effort));
+        });
+        traversal.children.forEach(childTraversal => this.flattenReportDataForTraversal(filterApplication, childTraversal));
+    }
+
+    private getFilterApplication(traversal:ProjectTraversalFullDTO):FilterApplication {
+        if (!this.filteredApplications)
+            return null;
+        console.log(" finding for: " + traversal.canonicalFilename + " among: ", this.filteredApplications);
+
+        return this.filteredApplications.find(app => app.fileName == traversal.canonicalFilename);
     }
 }
