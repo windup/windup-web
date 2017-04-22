a0937164f67b3e313c6bf7fef471ac9faf8c9b99
Ondrej Zizka <zizka@seznam.cz>
2017-04-22 03:15:39 +0200

    T778 about page - add Links and Contributors

--- ui/src/main/webapp/src/app/misc/about.component.ts
+++ ui/src/main/webapp/src/app/misc/about.component.ts
@@ -1,9 +1,30 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, AfterViewInit} from "@angular/core";
 
 @Component({
     templateUrl: './about.component.html',
+    styles: [`
+        /deep/ img.windupContributor {
+            width: 35px;
+            margin: 4px;
+        }
+    `]
 })
-export class AboutPageComponent implements OnInit {
+export class AboutPageComponent implements OnInit, AfterViewInit {
+
     ngOnInit():any {
     }
+
+    ngAfterViewInit(): void {
+        var divTarget = $("#windup-contributors");
+        $.getJSON( "https://api.github.com/repos/windup/windup/contributors", function( data ) {
+            $.each( data, function( key, val ) {
+                $( "<a data-toggle='tooltip' title='"+val.login+"' href='"+val.html_url+"'><img class='windupContributor' sr"+"c='"+val.avatar_url+"'/></a>").appendTo(divTarget);
+            });
+        });
+
+        $('[data-toggle="tooltip"]').tooltip({
+            'placement': 'top'
+        });
+    }
+
 }
