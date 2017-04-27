0f42523e1740ac55c968305547815808d8f81bb3
mrizzi <mrizzi@redhat.com>
2017-04-27 17:58:42 +0200

    T881 Add Run Analysis Button: fixes

--- ui/src/main/webapp/src/app/executions/executions-list.component.ts
+++ ui/src/main/webapp/src/app/executions/executions-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from "@angular/core";
 import {WindupService} from "../services/windup.service";
-import {WindupExecution, AnalysisContext, PackageMetadata, Package, RegisteredApplication} from "windup-services";
+import {WindupExecution, AnalysisContext, MigrationPath} from "windup-services";
 import {NotificationService} from "../core/notification/notification.service";
 import {utils} from '../shared/utils';
 import {SortingService, OrderDirection} from "../shared/sort/sorting.service";
@@ -8,14 +8,10 @@ import {MigrationProjectService} from "../project/migration-project.service";
 import {MigrationProject} from "windup-services";
 import {WindupExecutionService} from "../services/windup-execution.service";
 import {ConfirmationModalComponent} from "../shared/dialog/confirmation-modal.component";
-import {RegisteredApplicationService} from "../registered-application/registered-application.service";
 import {AnalysisContextService} from "../analysis-context/analysis-context.service";
 import {Subscription} from "rxjs";
 import {ActivatedRoute, Router, NavigationEnd} from "@angular/router";
 import {RouteFlattenerService} from "../core/routing/route-flattener.service";
-import {forkJoin} from "rxjs/observable/forkJoin";
-import {AnalysisContextFormComponent} from "../analysis-context/analysis-context-form.component";
-import {PackageRegistryService} from "../analysis-context/package-registry.service";
 
 @Component({
     selector: 'wu-executions-list',
@@ -51,11 +47,7 @@ export class ExecutionsListComponent implements OnInit, OnDestroy {
     analysisContext: AnalysisContext;
     private routerSubscription: Subscription;
     project: MigrationProject;
-    packageTree: Package[] = [];
-    packageTreeLoaded: boolean = false;
-    includePackages: Package[];
-    excludePackages: Package[];
-    availableApps: RegisteredApplication[];
+    static DEFAULT_MIGRATION_PATH: MigrationPath = <MigrationPath>{ id: 101 };
 
     constructor(
         private _elementRef: ElementRef,
@@ -66,12 +58,9 @@ export class ExecutionsListComponent implements OnInit, OnDestroy {
         private _windupExecutionService: WindupExecutionService,
         private _analysisContextService: AnalysisContextService,
         private _migrationProjectService: MigrationProjectService,
-        private _appService: RegisteredApplicationService,
         private _router: Router,
         private _routeFlattener: RouteFlattenerService,
         private _activatedRoute: ActivatedRoute,
-        private _registeredApplicationService: RegisteredApplicationService,
-        private _packageRegistryService: PackageRegistryService
     ) {
         this.element = _elementRef.nativeElement;
     }
@@ -103,19 +92,12 @@ export class ExecutionsListComponent implements OnInit, OnDestroy {
                     .subscribe(context => {
                         this.analysisContext = context;
                         if (this.analysisContext.migrationPath == null)
-                            this.analysisContext.migrationPath = AnalysisContextFormComponent.DEFAULT_MIGRATION_PATH;
+                            this.analysisContext.migrationPath = ExecutionsListComponent.DEFAULT_MIGRATION_PATH;
                     });
 
                 // Reload the App from the service to ensure fresh data
                 this._migrationProjectService.get(project.id).subscribe(loadedProject => {
                     this.project = loadedProject;
-                    this.loadPackageMetadata();
-                });
-
-                // Load the apps of this project.
-                this._appService.getApplicationsByProjectID(project.id).subscribe(apps => {
-                    this.availableApps = apps;
-                    this.analysisContext.applications = apps.slice();
                 });
             }
         });
@@ -242,66 +224,5 @@ export class ExecutionsListComponent implements OnInit, OnDestroy {
                     this._notificationService.error(utils.getErrorMessage(error));
                 }
             );
-    }
-
-    private loadPackageMetadata() {
-        let registeredPackagesObservables = this.project.applications.map(app => {
-            return this._registeredApplicationService.waitUntilPackagesAreResolved(app);
-        });
-
-        /*
-         * TODO: Find out how to continuously update data with new information. (If it is worth it)
-         * (only huge issue is with same packages possibly having different Ids since they come from different apps)
-         *
-         * Idea is following:
-         *  Make a request to all applications to get PackageMetadata. Filter out finished ones and build package tree from them.
-         *  Fire new event on subject.
-         *  Make additional requests to all not yet finished applications (with some delay).
-         *  Every time some application finishes, add data to package tree.
-         *  Repeat until all packages are resolved :)
-         *
-         *  Basically pretty much everything is done now, only thing to do would be replacing forkJoin with forEach
-         *    and subscribing to every single observable separately.
-         *
-         *  Another thing to solve would be how to inform user about packages being only partial data
-
-         let countRemainingPackagesToBeResolved = registeredPackagesObservables.length;
-
-         let packageTree = [];
-
-         registeredPackagesObservables.forEach(observable => {
-         observable.subscribe(packageMetadata => {
-         countRemainingPackagesToBeResolved--;
-
-         packageTree = this._packageRegistryService.mergePackageRoots([ ... packageTree, ... packageMetadata.packageTree ]);
-         });
-         });
-         */
-
-        forkJoin(registeredPackagesObservables).subscribe((packageMetadataArray: PackageMetadata[]) => {
-            let arrayOfRoots = [].concat(...packageMetadataArray.map((singlePackageMetadata) => singlePackageMetadata.packageTree));
-            let mergedRoots = this._packageRegistryService.mergePackageRoots(arrayOfRoots);
-            mergedRoots.forEach(singleRoot => this._packageRegistryService.putHierarchy(singleRoot));
-
-            this.packageTree = mergedRoots;
-            this.packageTreeLoaded = true;
-
-            if (this.analysisContext != null) {
-                if (this.analysisContext.includePackages == null || this.analysisContext.includePackages.length == 0) {
-                    this.includePackages = [];
-                } else {
-                    this.includePackages = this.analysisContext.includePackages.map(node => this._packageRegistryService.get(node.id));
-                }
-
-                if (this.analysisContext.excludePackages == null || this.analysisContext.excludePackages.length == 0) {
-                    this.analysisContext.excludePackages = [];
-                } else {
-                    this.analysisContext.excludePackages = this.analysisContext.excludePackages.map(node => this._packageRegistryService.get(node.id));
-                }
-            }
-
-            this.includePackages = this.analysisContext.includePackages;
-            this.excludePackages = this.analysisContext.excludePackages;
-        });
     }
 }
