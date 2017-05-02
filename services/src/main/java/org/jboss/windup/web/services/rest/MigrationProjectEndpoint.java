0e4f6d00a5de918176c4088676deba39ccfc232c
Ondrej Zizka <zizka@seznam.cz>
2017-05-02 17:03:45 +0200

    S757 leftovers - cleaned (#376)

--- services/src/main/java/org/jboss/windup/web/services/rest/MigrationProjectEndpoint.java
+++ services/src/main/java/org/jboss/windup/web/services/rest/MigrationProjectEndpoint.java
@@ -11,6 +11,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 /**
  * Provides a service for creating, updating, and deleting migration projects.
