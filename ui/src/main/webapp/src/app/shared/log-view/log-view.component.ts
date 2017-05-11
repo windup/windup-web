e992602ab754c6b4550ec6213e35936b8e692968
mrizzi <mrizzi@redhat.com>
2017-05-11 17:07:11 +0200

    T871 Analysis details - Log tab: add follow/unfollow buttons

--- ui/src/main/webapp/src/app/shared/log-view/log-view.component.ts
+++ ui/src/main/webapp/src/app/shared/log-view/log-view.component.ts
@@ -1,4 +1,6 @@
-import {Component, ChangeDetectionStrategy, Input} from "@angular/core";
+import {
+    Component, ChangeDetectionStrategy, Input, AfterViewChecked, ViewChild, ElementRef, OnInit
+} from "@angular/core";
 
 @Component({
     templateUrl: './log-view.component.html',
@@ -6,14 +8,44 @@ import {Component, ChangeDetectionStrategy, Input} from "@angular/core";
     selector: 'wu-log-view',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LogViewComponent {
+export class LogViewComponent implements AfterViewChecked {
     private _text: string;
+
+    @ViewChild('logview') private myScrollContainer: ElementRef;
+    autoScrollActive: boolean = false;
 
     @Input()
     lines: string[] = [ "Loading..." ];
 
     constructor() {
 
+    }
+
+    ngAfterViewChecked() {
+        if (this.autoScrollActive) this.goToBottom();
+    }
+
+    goToBottom(): void {
+        /* TO-DO
+         Only FF supports scrollIntoViewOptions to get a better scrolling behaviour.
+         https://developer.mozilla.org/en/docs/Web/API/Element/scrollIntoView#Browser_compatibility
+         if (this.autoScrollActive) this.myScrollContainer.nativeElement.scrollIntoView({block: "end", behavior: "smooth"});
+         */
+        this.myScrollContainer.nativeElement.scrollIntoView(false);
+    }
+
+    toggleAutoScroll(): void {
+        this.autoScrollActive = !this.autoScrollActive;
+        if (this.autoScrollActive) this.goToBottom();
+    }
+
+    onScrollTop(): void {
+        this.autoScrollActive = false;
+        window.scrollTo(null, 0);
+    }
+
+    isOnTop(): boolean {
+        return document.body.scrollTop == 0;
     }
 
     @Input()
