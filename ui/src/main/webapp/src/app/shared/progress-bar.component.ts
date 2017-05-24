5c324b7b7b1bdbe81bf216224dfe8fd1c99ba324
Marco Rizzi <mrizzi@users.noreply.github.com>
2017-05-24 19:22:39 +0200

    T968 Analysis list: fix the active analysis progress bars to not change page too dramatically (#430)
    * T968 Analysis list: fix the active analysis progress bars to not change page too dramatically
    
    * T968 Analysis list: fix the active analysis progress bars

--- ui/src/main/webapp/src/app/shared/progress-bar.component.ts
+++ ui/src/main/webapp/src/app/shared/progress-bar.component.ts
@@ -4,21 +4,22 @@ import {Inject, Input} from '@angular/core';
 @Component({
     selector: 'wu-progress-bar',
     template: `
-        <div class="progress-description">
-            <div class="spinner spinner-xs spinner-inline"></div>&nbsp;<strong i18n="Analysis id">Analysis:</strong> #{{activeExecutionId}}&nbsp;&nbsp;<strong i18n="Progressbar task">Task:</strong> {{taskName ? taskName : "Starting..."}}
+        <div class="progress-container progress-description-left" style="padding-left: 135px;">
+        <div class="progress-description" style="max-width: 130px;">
+            <div class="spinner spinner-xs spinner-inline"></div>&nbsp;<strong i18n="Analysis id">Analysis:</strong> #{{activeExecutionId}}
         </div>
-        <div class="progress progress-label-top-right">
+        <div class="progress" style="margin-bottom: 0px;">
             <div
                     class="progress-bar" role="progressbar"
                     aria-valuemin="0"
                     [attr.aria-valuenow]="currentValue"
                     [attr.aria-valuemax]="maxValue"
                     [style.width]="(currentValue/maxValue)*100 + '%'">
-                <span>
-                    {{currentValue ? currentValue : "?"}}/{{maxValue ? maxValue : "?"}}
+                <span style="position: absolute; display: block; width: 100%; color: #363636;">
+                    <strong i18n="Progressbar task">Task:</strong> {{taskName ? taskName : "Starting..."}} ({{currentValue ? currentValue : "?"}}/{{maxValue ? maxValue : "?"}})
                 </span>
             </div>
-        </div>`
+        </div></div>`
 })
 export class ProgressBarComponent {
 
