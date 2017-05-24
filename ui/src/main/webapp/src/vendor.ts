1d171b6aff75cc9118b59f04677436430b672abd
David Klingenberg <dklingen@redhat.com>
2017-05-24 13:40:51 +0200

    Update AOT webpack configuration.
    Fix code splitting for polyfills.
    
    Polyfills are kept in polyfills file.
    
    Fix webpack bug.
    
    Update chunks ordering - order must be polyfills, vendor, app.

--- ui/src/main/webapp/src/vendor.ts
+++ ui/src/main/webapp/src/vendor.ts
@@ -16,16 +16,6 @@ import 'd3';
 import 'd3-selection-multi';
 */
 
-// Angular 2
-import "@angular/platform-browser";
-import "@angular/platform-browser-dynamic";
-import "@angular/core";
-import "@angular/common";
-import "@angular/http";
-import "@angular/router";
-
-import "@angularclass/hmr";
-
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 
