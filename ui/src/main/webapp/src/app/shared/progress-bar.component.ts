c82556f9d2af3a1e77c074301bce5e7c7f3621e6
Marco Rizzi <mrizzi@users.noreply.github.com>
2017-05-31 22:36:47 +0200

    T1023 Active analysis: fix progress bar text (#444)

--- ui/src/main/webapp/src/app/shared/progress-bar.component.ts
+++ ui/src/main/webapp/src/app/shared/progress-bar.component.ts
@@ -8,16 +8,15 @@ import {Inject, Input} from '@angular/core';
         <div class="progress-description" style="max-width: 130px;">
             <div class="spinner spinner-xs spinner-inline"></div>&nbsp;<strong i18n="Analysis id">Analysis:</strong> #{{activeExecutionId}}
         </div>
-        <div class="progress" style="margin-bottom: 0px;">
+        <div class="progress" style="margin-bottom: 0px; margin-right: 0px;">
             <div
                     class="progress-bar" role="progressbar"
                     aria-valuemin="0"
                     [attr.aria-valuenow]="currentValue"
                     [attr.aria-valuemax]="maxValue"
-                    [style.width]="(currentValue/maxValue)*100 + '%'">
-                <span style="position: absolute; display: block; width: 100%; color: #363636;">
+                    [style.width]="(currentValue/maxValue)*100 + '%'"
+                    style="color: #363636; text-align: left; white-space: nowrap; padding-left: 5px;">
                     <strong i18n="Progressbar task">Task:</strong> {{taskName ? taskName : "Starting..."}} ({{currentValue ? currentValue : "?"}}/{{maxValue ? maxValue : "?"}})
-                </span>
             </div>
         </div></div>`
 })
