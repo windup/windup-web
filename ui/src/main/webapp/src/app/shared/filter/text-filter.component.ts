2b12c2590fb5eec6b32e07988feba6e7fedb96cf
David Klingenberg <dklingen@redhat.com>
2017-06-06 15:48:05 +0200

    Add filtering and sorting to rules configuration
    WINDUP-1419
    
    Add empty filters message

---
+++ ui/src/main/webapp/src/app/shared/filter/text-filter.component.ts
@@ -0,0 +1,62 @@
+import {
+    AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input,
+    Output
+} from "@angular/core";
+
+@Component({
+    selector: 'wu-text-filter',
+    templateUrl: './text-filter.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TextFilterComponent implements AfterViewInit {
+    _filterableAttributes: string[] = [];
+
+    @Output()
+    setFilter = new EventEmitter<FilterOption>();
+
+    selectedAttribute: string = '';
+
+    filterText: string = '';
+
+    public constructor(protected _element: ElementRef) {
+    }
+
+    ngAfterViewInit(): void {
+        $(this._element.nativeElement).find('.dropdown-toggle').dropdown();
+    }
+
+    @Input()
+    public set filterableAttributes(attributes: string[]) {
+        if (attributes) {
+            this._filterableAttributes = attributes;
+            this.selectedAttribute = attributes[0];
+        }
+    }
+
+    public get filterableAttributes() {
+        return this._filterableAttributes;
+    }
+
+    public selectAttribute(attribute: string) {
+        this.selectedAttribute = attribute;
+    }
+
+    public addFilter() {
+        this.setFilter.next({
+            name: this.selectedAttribute,
+            value: this.filterText
+        });
+        this.resetFilter();
+    }
+
+    public resetFilter() {
+        this.filterText = '';
+    }
+}
+
+export interface FilterOption {
+    name: string;
+    value: string;
+    field?: string;
+    callback?: (item: any) => boolean;
+}
---
+++ ui/src/main/webapp/src/app/shared/toolbar/toolbar.component.ts
@@ -0,0 +1,56 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from "@angular/core";
+import {SortOption} from "../sort/sort.component";
+import {OrderDirection} from "../sort/sorting.service";
+import {FilterOption} from "../filter/text-filter.component";
+
+export interface SortConfiguration {
+    sortOptions: SortOption[];
+    selectedOption: SortOption;
+    direction: OrderDirection;
+}
+
+export interface FilterConfiguration {
+    filterOptions: FilterOption[];
+    selectedFilters: FilterOption[];
+    countFilteredItems: number;
+}
+
+@Component({
+    selector: 'wu-toolbar',
+    templateUrl: './toolbar.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ToolbarComponent {
+
+    @Input()
+    sortConfiguration: SortConfiguration;
+
+    @Input()
+    filterConfiguration: FilterConfiguration;
+
+    @Output()
+    filterChange = new EventEmitter();
+
+    @Output()
+    sortChange = new EventEmitter();
+
+    updateSort() {
+        this.sortChange.emit(this.sortConfiguration);
+    }
+
+    addFilter(filter: FilterOption) {
+        this.filterConfiguration.selectedFilters = [
+            ...this.filterConfiguration.selectedFilters.filter(item => item.name !== filter.name),
+            filter
+        ];
+        this.updateFilters();
+    }
+
+    updateFilters() {
+        this.filterChange.next(this.filterConfiguration.selectedFilters);
+    }
+
+    isFilterActive() {
+        return this.filterConfiguration.selectedFilters.length > 0;
+    }
+}
