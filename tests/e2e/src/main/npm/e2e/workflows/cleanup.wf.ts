59e1814f15531573247a0ec6e0dfc3fabdd02480
David Klingenberg <dklingen@redhat.com>
2017-10-06 11:03:06 +0200

    Update cleanup - add deleteProjectFromList

--- tests/e2e/src/main/npm/e2e/workflows/cleanup.wf.ts
+++ tests/e2e/src/main/npm/e2e/workflows/cleanup.wf.ts
@@ -2,20 +2,32 @@ import {Project, ProjectPage} from "../pages/project.po";
 import {ConfirmDialogPage} from "../pages/confirm-dialog.po";
 
 export class CleanupWorkflow {
-    public deleteProject(name: string) {
+    protected loadProject(name: string): Promise<Project> {
         const projectPage = new ProjectPage();
-        let projectList;
 
-        return projectPage.navigateTo()
-            .then(() => {
-                projectPage.getProjectList().then(projects => projectList = projects);
-            })
-            .then(() => this.deleteProjectFromList(name, projectList));
+        let projectList;
+        return projectPage.navigateTo().then(() => {
+            projectPage.getProjectList().then(projects => projectList = projects)
+        }).then(() => {
+            return projectList.find(item => item.name === name);
+        });
     }
 
     public deleteProjectFromList(name: string, projectList: Project[]) {
-        let project = projectList.find(item => item.name === name);
+        const project = projectList.find(item => item.name === name);
+        return this.deleteProject(project);
+    }
 
+    public deleteProject(project: string|Project) {
+        if (typeof project === 'string') {
+            return this.loadProject(project)
+                .then(projectObject => this.doDeleteProject(projectObject));
+        } else {
+            return this.doDeleteProject(project);
+        }
+    }
+
+    protected doDeleteProject(project: Project): Promise<any> {
         if (project != null) {
             return project.deleteButton.click().then(() => {
                 const confirmDialog = new ConfirmDialogPage();
