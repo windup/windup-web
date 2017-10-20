02a64aa07deba020e626d910d841e480edf87cd3
David Klingenberg <dklingen@redhat.com>
2017-10-20 11:19:15 +0200

    Add windup-web.h2 database with existing project and analysis
    Add copy tasks
    
    Update pom.xml - unzip reports

---
+++ tests/e2e/src/main/npm/e2e/utils/data.ts
@@ -0,0 +1,14 @@
+export const PROJECT_WITH_ANALYSIS = {
+    id: 1355,
+    name: 'Project with analysis',
+    executions: [
+        {
+            id: 1
+        }
+    ]
+};
+
+export const PROJECT_WITHOUT_ANALYSIS = {
+    id: 7905,
+    name: 'Project without analysis'
+};
