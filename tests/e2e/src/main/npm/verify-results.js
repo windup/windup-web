8125a27b361bc039361c1610f5b426333d5d4ef5
David Klingenberg <dklingen@redhat.com>
2017-10-30 14:09:53 +0100

    Use maven failsafe plugin
    Use failsafe plugin to run integration tests.
    - pre-integration-test phase starts server
    - interation-test phase executes the tests
    - post-integration-test phase stops server
    
    There are several workarounds required to do that:
    - maven exec of protractor tests must accept failure status code (1) as
    success
    - there must be another script, which parses test-results.xml and
    creates failsafe-report.xml from it
    - failsafe then runs verify phase and checks failsafe report. If test
    failures are found, it fails the maven execution safely (it runs
    post-integration-test phase and stops server first)

---
+++ tests/e2e/src/main/npm/verify-results.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const xpath = require('xpath');
+const DOMParser = require('xmldom').DOMParser;
+const fs = require('fs');
+const path = require("path");
+
+function makeDirIfNotExists(dirPath) {
+    if (!fs.existsSync(dirPath)) {
+        const parent = path.dirname(dirPath);
+
+        makeDirIfNotExists(parent);
+
+        fs.mkdirSync(dirPath);
+    }
+}
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'test';
+
+if (process.argv.length < 4) {
+    console.error('Expecting <test-results.xml path> and <failsafe-report.xml path> arguments');
+    process.exit(1);
+}
+
+const testResultsFile = process.argv[2];
+const failsafeResultsFile = process.argv[3];
+
+if (!fs.existsSync(testResultsFile)) {
+    console.error('Test results file does not exist');
+    process.exit(1);
+}
+
+const testResultsXLM = fs.readFileSync(testResultsFile, 'utf8');
+
+const document = new DOMParser().parseFromString(testResultsXLM, 'text/xml');
+
+const countErrors = xpath.evaluate('count(//testcase/error)', document, null, xpath.XPathResult.NUMBER_TYPE, null).numberValue;
+const countFailures = xpath.evaluate('count(//testcase/failure)', document, null, xpath.XPathResult.NUMBER_TYPE, null).numberValue;
+const countSkipped = xpath.evaluate('count(//testcase/skipped)', document, null, xpath.XPathResult.NUMBER_TYPE, null).numberValue;
+const countTestCases = xpath.evaluate('count(//testcase)', document, null, xpath.XPathResult.NUMBER_TYPE, null).numberValue;
+
+const failsafeSummary = "<failsafe-summary>\n\
+    <completed>" + countTestCases + "</completed>\n\
+    <errors>" + countErrors + "</errors>\n\
+    <failures>" + countFailures + "</failures>\n\
+    <skipped>" + countSkipped + "</skipped>\n\
+</failsafe-summary>\n";
+
+makeDirIfNotExists(failsafeResultsFile);
+
+fs.writeFileSync(failsafeResultsFile, failsafeSummary);
+
