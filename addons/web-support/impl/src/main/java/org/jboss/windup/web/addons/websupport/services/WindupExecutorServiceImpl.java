ee89d770d8c10db4f6ae277b70fff7a55b1f2a60
Marco Rizzi <mrizzi@users.noreply.github.com>
2017-11-15 20:57:37 +0100

    WINDUP-1749: Reduce the loggin (#532) (#534)
    * WINDUP-1749: Reduce the loggin (#532)
    
    * WINDUP-1749: Reduce the logging (not too much)

--- addons/web-support/impl/src/main/java/org/jboss/windup/web/addons/websupport/services/WindupExecutorServiceImpl.java
+++ addons/web-support/impl/src/main/java/org/jboss/windup/web/addons/websupport/services/WindupExecutorServiceImpl.java
@@ -91,7 +91,7 @@ public class WindupExecutorServiceImpl implements WindupExecutorService
             configuration.setOptionValue(optionEntry.getKey(), optionEntry.getValue());
         }
         
-        Logger globalLogger = Logger.getLogger("org.jboss.windup");
+        Logger globalLogger = Logger.getLogger("org.jboss.windup.web.services.WindupWebProgressMonitor");
         Handler logHandler = null;
         try
         {
