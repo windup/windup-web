73022b076b2fdb24beec087811df7f735b704099
David Klingenberg <dklingenberg@gmail.com>
2017-11-27 13:07:46 +0100

    Remove contributors from about (#538)

--- ui/src/main/webapp/src/app/misc/about.component.ts
+++ ui/src/main/webapp/src/app/misc/about.component.ts
@@ -1,17 +1,11 @@
-import {Component, OnInit, AfterViewInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Constants} from "../constants";
 import {Http} from "@angular/http";
 
 @Component({
-    templateUrl: './about.component.html',
-    styles: [`
-        /deep/ img.windupContributor {
-            width: 35px;
-            margin: 4px;
-        }
-    `]
+    templateUrl: './about.component.html'
 })
-export class AboutPageComponent implements OnInit, AfterViewInit {
+export class AboutPageComponent implements OnInit {
 
     private WINDUP_CORE_VERSION_URL: string = "/windup/coreVersion";
 
@@ -19,9 +13,6 @@ export class AboutPageComponent implements OnInit, AfterViewInit {
     scmRevisionWindupWeb: string = Constants.WINDUP_WEB_SCM_REVISION;
     versionWindupCore: string = "(loading)";
     scmRevisionWindupCore: string = "(loading)";
-
-    // Externally loaded
-    contributors: {login: string, html_url: string; avatar_url: string }[] = [];
 
     constructor (private _http: Http) {
     }
@@ -34,24 +25,5 @@ export class AboutPageComponent implements OnInit, AfterViewInit {
                 this.versionWindupCore = versionAndRevision.version;
                 this.scmRevisionWindupCore = versionAndRevision.scmRevision;
             });
-
-        /* TODO: This is getting Unauthorized.
-        this._http.get("https://api.github.com/repos/windup/windup/contributors")
-            .map(res => res.json())
-            .subscribe(contributors => this.contributors = contributors);
-        */
-    }
-
-    ngAfterViewInit(): void {
-        let divTarget = $("#windup-contributors");
-        $.getJSON( "https://api.github.com/repos/windup/windup/contributors", function( data ) {
-            $.each( data, function( key, val ) {
-                $( "<a data-toggle='tooltip' title='"+val.login+"' href='"+val.html_url+"'><img class='windupContributor' sr"+"c='"+val.avatar_url+"'/></a>").appendTo(divTarget);
-            });
-        });
-
-        $('[data-toggle="tooltip"]').tooltip({
-            'placement': 'top'
-        });
     }
 }
