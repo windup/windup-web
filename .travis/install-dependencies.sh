7c2330f78f05bd80a9b2a47e7ea7af69c2a37bf1
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-03-08 09:29:06 +0100

    add dependencies

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -2,7 +2,15 @@
 
 echo "Executing before script created script"
 
+function bell() {
+  while true; do
+    echo -e "\a"
+    sleep 60
+  done
+}
+bell &
 for GIT_REPO in windup windup-rulesets windup-distribution ; do
     git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/${GIT_REPO}.git"
     mvn -f ${GIT_REPO}/pom.xml clean install -DskipTests
 done
+exit $?
