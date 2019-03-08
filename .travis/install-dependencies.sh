7c4d2647ae4b2dbfa7000f1c7e66b4d51a33772c
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-03-08 10:34:09 +0100

    add dependencies

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -3,16 +3,16 @@
 # For avoiding travis timeout
 function bell() {
   while true; do
-    echo -e "Waiting... \n"
+    echo -e "..."
     sleep 60
   done
 }
 bell &
 for GIT_REPO in windup windup-rulesets windup-distribution ; do
-    echo "git clone --branch=${DEFAULT_BRANCH} https://github.com/${GIT_ORGANIZATION}/${GIT_REPO}.git"
+    echo "$ git clone --branch=${DEFAULT_BRANCH} https://github.com/${GIT_ORGANIZATION}/${GIT_REPO}.git"
     git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/${GIT_REPO}.git"
 
-    echo "mvn -f ${GIT_REPO}/pom.xml clean install -DskipTests >> install_dependencies.log 2>&1"
+    echo "$ mvn -f ${GIT_REPO}/pom.xml clean install -DskipTests >> install_dependencies.log 2>&1"
     mvn -f ${GIT_REPO}/pom.xml clean install -DskipTests >> install_dependencies.log 2>&1
 
     tail --lines=100 install_dependencies.log
