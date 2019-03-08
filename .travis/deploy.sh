54478eb1a1c9ec9e6b7fc358f3e0503c51dccf6a
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-03-08 15:22:52 +0100

    change colors to patternfly original colors

---
+++ .travis/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+GIT_BRANCH="$TRAVIS_BRANCH"
+GIT_COMMIT_ID="$(git rev-parse --short HEAD)"
+DOCKER_TAG="${GIT_BRANCH}_${GIT_COMMIT_ID}"
+
+echo "Logging..."
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
+echo "Tagging using DOCKER_TAG=${DOCKER_TAG}..."
+docker tag ${DOCKER_ID}/windup-web-openshift ${DOCKER_ID}/windup-web-openshift:${DOCKER_TAG}
+docker tag ${DOCKER_ID}/windup-web-openshift-me ${DOCKER_ID}/windup-web-openshift-me:${DOCKER_TAG}
+
+echo "Pushing images..."
+docker push ${DOCKER_ID}/windup-web-openshift:${DOCKER_TAG}
+docker push ${DOCKER_ID}/windup-web-openshift-me:${DOCKER_TAG}
