8ed31a7619d6fcd976296138b72a85349a84e2e3
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-03-11 16:35:13 +0100

    remove cache for maven

--- .travis/build_docker_images.sh
+++ .travis/build_docker_images.sh
@@ -1,36 +1,29 @@
 #!/usr/bin/env bash
-
 # Install windup-openshift
-echo "$ git clone --branch=${DEFAULT_BRANCH} https://github.com/${GIT_ORGANIZATION}/windup-openshift.git"
-git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/windup-openshift.git"
+git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/windup-openshift.git" "${HOME}/dependencies/windup-openshift"
 
-mvn -f windup-openshift clean install -DskipTests \
+mvn -f ${HOME}/dependencies/windup-openshift clean install -DskipTests \
 -Ddocker.name.windup.web=${DOCKER_ID}/windup-web-openshift \
 -Ddocker.name.windup.web.executor=${DOCKER_ID}/windup-web-openshift-me \
 >> windup_openshift.log 2>&1
 
-echo "$ tail --lines=50 windup_openshift.log"
 tail --lines=50 windup_openshift.log
 
 
 # Install windup-web-distribution
-echo "$ git clone --branch=${DEFAULT_BRANCH} https://github.com/${GIT_ORGANIZATION}/windup-web-distribution.git"
-git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/windup-web-distribution.git"
+git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/windup-web-distribution.git" "${HOME}/dependencies/windup-web-distribution"
 
-mvn -f windup-web-distribution/pom.xml clean install -DskipTests \
+mvn -f ${HOME}/dependencies/windup-web-distribution/pom.xml clean install -DskipTests \
 >> windup_web_distribution.log 2>&1
 
-echo "$ tail --lines=50 windup_web_distribution.log"
 tail --lines=50 windup_web_distribution.log
 
 
 # Install windup-docker
-echo "$ git clone --branch=${DEFAULT_BRANCH} https://github.com/${GIT_ORGANIZATION}/windup-docker.git"
-git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/windup-docker.git"
+git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/windup-docker.git" "${HOME}/dependencies/windup-docker"
 
-mvn -f windup-docker/pom.xml clean install -DskipTests \
+mvn -f ${HOME}/dependencies/windup-docker/pom.xml clean install -DskipTests \
 -Ddocker.name.windup.web=${DOCKER_ID}/windup-web \
 >> windup_docker.log 2>&1
 
-echo "$ tail --lines=50 windup_docker.log"
 tail --lines=50 windup_docker.log
--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 for GIT_REPO in windup windup-rulesets windup-distribution ; do
-    echo "$ git clone --branch=${DEFAULT_BRANCH} https://github.com/${GIT_ORGANIZATION}/${GIT_REPO}.git"
-    git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/${GIT_REPO}.git"
+    echo "$ git clone --branch=${DEFAULT_BRANCH} https://github.com/${GIT_ORGANIZATION}/${GIT_REPO}.git ${HOME}/dependencies/${GIT_REPO}"
+    git clone --branch="${DEFAULT_BRANCH}" "https://github.com/${GIT_ORGANIZATION}/${GIT_REPO}.git" "${HOME}/dependencies/${GIT_REPO}"
 
-    echo "$ mvn -f ${GIT_REPO}/pom.xml clean install -DskipTests >> install_dependencies.log 2>&1"
-    mvn -f ${GIT_REPO}/pom.xml clean install -DskipTests >> install_dependencies.log 2>&1
+    echo "$ mvn -f ${HOME}/dependencies/${GIT_REPO}/pom.xml clean install -DskipTests >> install_dependencies.log 2>&1"
+    mvn -f ${HOME}/dependencies/${GIT_REPO}/pom.xml clean install -DskipTests >> install_dependencies.log 2>&1
 
     echo "$ tail --lines=50 install_dependencies.log"
     tail --lines=50 install_dependencies.log
