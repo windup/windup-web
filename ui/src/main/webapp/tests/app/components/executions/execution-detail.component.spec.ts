98eea3b96a0c0c1158d53c9e49370b65cba3a90e
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2019-12-02 18:52:46 +0100

    cover test for natural order

--- ui/src/main/webapp/tests/app/components/executions/execution-detail.component.spec.ts
+++ ui/src/main/webapp/tests/app/components/executions/execution-detail.component.spec.ts
@@ -88,7 +88,17 @@ describe('ExecutionDetailComponent', () => {
         }, {
             id: 5,
             version: null,
-            path: null,
+            path: 'packageB',
+            scanRecursively: null,
+            shortPath: null,
+            loadError: null,
+            rulesPathType: 'USER_PROVIDED',
+            registrationType: 'UPLOADED',
+            scopeType: 'PROJECT'
+        }, {
+            id: 6,
+            version: null,
+            path: 'packageA',
             scanRecursively: null,
             shortPath: null,
             loadError: null,
@@ -101,11 +111,12 @@ describe('ExecutionDetailComponent', () => {
         sortRulesPathsByPriority(rulesPath);
 
         // then
-        expect(rulesPath[0].id).toEqual(5);
-        expect(rulesPath[1].id).toEqual(4);
-        expect(rulesPath[2].id).toEqual(3);
-        expect(rulesPath[3].id).toEqual(2);
-        expect(rulesPath[4].id).toEqual(1);
+        expect(rulesPath[0].id).toEqual(6);
+        expect(rulesPath[1].id).toEqual(5);
+        expect(rulesPath[2].id).toEqual(4);
+        expect(rulesPath[3].id).toEqual(3);
+        expect(rulesPath[4].id).toEqual(2);
+        expect(rulesPath[5].id).toEqual(1);
     });
 
     it('should order labelsPath by labelsPathType, scopeType, and registrationType', () => {
@@ -154,7 +165,17 @@ describe('ExecutionDetailComponent', () => {
         }, {
             id: 5,
             version: null,
-            path: null,
+            path: 'packageB',
+            scanRecursively: null,
+            shortPath: null,
+            loadError: null,
+            labelsPathType: 'USER_PROVIDED',
+            registrationType: 'UPLOADED',
+            scopeType: 'PROJECT'
+        }, {
+            id: 6,
+            version: null,
+            path: 'packageA',
             scanRecursively: null,
             shortPath: null,
             loadError: null,
@@ -167,10 +188,11 @@ describe('ExecutionDetailComponent', () => {
         sortLabelsPathsByPriority(labelsPath);
 
         // then
-        expect(labelsPath[0].id).toEqual(5);
-        expect(labelsPath[1].id).toEqual(4);
-        expect(labelsPath[2].id).toEqual(3);
-        expect(labelsPath[3].id).toEqual(2);
-        expect(labelsPath[4].id).toEqual(1);
+        expect(labelsPath[0].id).toEqual(6);
+        expect(labelsPath[1].id).toEqual(5);
+        expect(labelsPath[2].id).toEqual(4);
+        expect(labelsPath[3].id).toEqual(3);
+        expect(labelsPath[4].id).toEqual(2);
+        expect(labelsPath[5].id).toEqual(1);
     });
 });
