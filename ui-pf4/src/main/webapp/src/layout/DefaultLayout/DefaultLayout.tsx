9d3ce6e4fd8ad99c6434793e510d5085af04eaa3
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-09-08 09:15:24 +0200

    Add patternfly 3 link

--- ui-pf4/src/main/webapp/src/layout/DefaultLayout/DefaultLayout.tsx
+++ ui-pf4/src/main/webapp/src/layout/DefaultLayout/DefaultLayout.tsx
@@ -1,36 +1,74 @@
 import * as React from "react";
-import { Page, SkipToContent } from "@patternfly/react-core";
+import { Page, SkipToContent, Button } from "@patternfly/react-core";
+import { Flex, FlexItem, Banner } from "@patternfly/react-core";
+import { TimesIcon } from "@patternfly/react-icons";
+
 import { HeaderApp } from "../HeaderApp";
 import { SidebarApp } from "../SidebarApp";
 
 export interface HorizontalLayoutProps {}
 
-interface State {}
+export const DefaultLayout: React.FC = ({ children }) => {
+  const [showBanner, setShowBanner] = React.useState(true);
 
-export class DefaultLayout extends React.Component<
-  HorizontalLayoutProps,
-  State
-> {
-  render() {
-    const { children } = this.props;
+  const pageId = "main-content-page-layout-horizontal-nav";
+  const PageSkipToContent = (
+    <SkipToContent href={`#${pageId}`}>Skip to content</SkipToContent>
+  );
 
-    const pageId = "main-content-page-layout-horizontal-nav";
-    const PageSkipToContent = (
-      <SkipToContent href={`#${pageId}`}>Skip to content</SkipToContent>
-    );
-
-    return (
-      <React.Fragment>
-        <Page
-          header={<HeaderApp />}
-          sidebar={<SidebarApp />}
-          isManagedSidebar
-          skipToContent={PageSkipToContent}
-          mainContainerId={pageId}
-        >
-          {children}
-        </Page>
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <Flex
+        direction={{ default: "column" }}
+        flexWrap={{ default: "nowrap" }}
+        spaceItems={{ default: "spaceItemsNone" }}
+        style={{ height: "100%" }}
+      >
+        {showBanner && (
+          <FlexItem>
+            <Banner variant="default" isSticky>
+              <Flex
+                justifyContent={{
+                  default: "justifyContentCenter",
+                  lg: "justifyContentSpaceBetween",
+                }}
+                flexWrap={{ default: "nowrap" }}
+              >
+                <div className="pf-u-display-none pf-u-display-block-on-lg"></div>
+                <div className="pf-u-display-none pf-u-display-block-on-lg">
+                  Continue using MTA based on&nbsp;
+                  <a href={`${window.location.origin}/mta-web`}>Patternfly 3</a>
+                </div>
+                <div className="pf-u-display-none-on-lg">
+                  Continue using MTA based on&nbsp;
+                  <a href={`${window.location.origin}/mta-web`}>Patternfly 3</a>
+                </div>
+                <div className="pf-u-display-none pf-u-display-block-on-lg">
+                  <Button
+                    variant="plain"
+                    aria-label="Action"
+                    onClick={() => setShowBanner(false)}
+                    style={{ padding: 0 }}
+                  >
+                    <TimesIcon />
+                  </Button>
+                </div>
+              </Flex>
+            </Banner>
+          </FlexItem>
+        )}
+        <FlexItem grow={{ default: "grow" }} style={{ minHeight: 0 }}>
+          <Page
+            header={<HeaderApp />}
+            sidebar={<SidebarApp />}
+            isManagedSidebar
+            skipToContent={PageSkipToContent}
+            mainContainerId={pageId}
+          >
+            {children}
+          </Page>
+        </FlexItem>
+      </Flex>
+    </React.Fragment>
+  );
+};
