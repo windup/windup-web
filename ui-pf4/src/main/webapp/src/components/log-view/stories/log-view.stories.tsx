9fc8fc99311cca3c3c03e3f0c050e21ccd5f90de
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-11-02 10:47:15 +0100

    Fix log-view storybook

--- ui-pf4/src/main/webapp/src/components/log-view/stories/log-view.stories.tsx
+++ ui-pf4/src/main/webapp/src/components/log-view/stories/log-view.stories.tsx
@@ -13,3 +13,8 @@ export default {
 const Template: Story<LogViewProps> = (args) => <LogView {...args} />;
 
 export const Basic = Template.bind({});
+Basic.args = {
+  headerText: "This is the header",
+  lines: ["line1", "line2", "line3"],
+  isStreamActive: true,
+};
