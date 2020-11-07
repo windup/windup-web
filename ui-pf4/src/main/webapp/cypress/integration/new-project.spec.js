77161d2d01e4f5352fe3f2421e7b7960e4d5e81d
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-11-07 22:55:21 +0100

    update e2e test

--- ui-pf4/src/main/webapp/cypress/integration/new-project.spec.js
+++ ui-pf4/src/main/webapp/cypress/integration/new-project.spec.js
@@ -32,8 +32,9 @@ context("New Project", () => {
      */
     cy.contains("Add applications");
 
-    const dropzoneSelector = "input[type='file']";
-    const applicationName = "1111-1.0-SNAPSHOT.zip";
+    const dropzoneSelector = ".upload-files-section__component__dropzone";
+    const application1 = "dwr.zip";
+    const application2 = "1111-1.0-SNAPSHOT.zip";
 
     verifyActionButtonsDisabled();
 
@@ -45,13 +46,17 @@ context("New Project", () => {
     cy.get(".pf-c-tabs__item").eq(0).click();
     verifyActionButtonsDisabled();
 
-    cy.get(dropzoneSelector).attachFile(applicationName);
+    cy.get(dropzoneSelector).attachFile(application1, {
+      subjectType: "drag-n-drop",
+    });
     verifyActionButtonsEnabled();
 
     cy.get("button.pf-c-button[aria-label=delete-application]").click();
     verifyActionButtonsDisabled();
 
-    cy.get(dropzoneSelector).attachFile(applicationName);
+    cy.get(dropzoneSelector).attachFile(application2, {
+      subjectType: "drag-n-drop",
+    });
     verifyActionButtonsEnabled();
 
     cy.get(".pf-c-button.pf-m-primary").contains("Next").click();
