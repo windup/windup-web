e4bd9bf221488d02ed4ef14b2154906cdc7e9159
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-11-16 11:35:21 +0100

    Add executions tests

--- ui-pf4/src/main/webapp/cypress/integration/analysis-results.spec.js
+++ ui-pf4/src/main/webapp/cypress/integration/analysis-results.spec.js
@@ -81,7 +81,10 @@ context("Analysis results", () => {
                 cy.request({
                   method: "PUT",
                   headers: headers,
-                  body: { ...analysisContext },
+                  body: {
+                    ...analysisContext,
+                    advancedOptions: [{ name: "target", value: "eap7" }],
+                  },
                   url: `${Cypress.env(
                     "MTA_API"
                   )}/analysis-context/migrationProjects/${
@@ -98,7 +101,51 @@ context("Analysis results", () => {
   it("Filtering", () => {
     cy.visit(`/projects-details/${PROJECT_ID}/analysis-results/executions`);
 
-    cy.get(".pf-c-empty-state").contains(
+    cy.get(".pf-c-empty-state", { timeout: 10000 }).contains(
+      "There are no analysis results for this project"
+    );
+
+    // Run analysis
+    cy.get(".pf-m-primary").contains("Run analysis").first().click();
+    cy.get("tbody > tr").should("have.length", 1);
+    ("");
+    // Check 'Active analysis'
+    cy.get("h1").contains("Active analysis", { timeout: 10_000 });
+    cy.get("tbody > tr").contains("Pending");
+    cy.get(".pf-c-progress", { timeout: 60_000 });
+    cy.get("tbody > tr").contains("Running");
+
+    cy.get("a[title='Reports']", { timeout: 90_000 });
+    cy.get("tbody > tr").contains("Completed");
+
+    // Check details
+    cy.get("tbody > tr > td > a").first().click();
+    cy.url().should("contains", "/overview");
+
+    cy.get(".pf-c-description-list__text", { timeout: 5000 }).contains(
+      "Target"
+    );
+    cy.get(".pf-c-description-list__text").contains("eap7");
+
+    cy.get(".pf-c-description-list__text").contains("Source");
+    cy.get(".pf-c-description-list__text").contains("Not defined");
+
+    cy.get(".pf-c-tabs__item").contains("Logs").click();
+    cy.get(".log-line-text", { timeout: 50_000 }).should(
+      "have.length.greaterThan",
+      10
+    );
+
+    // Go back to table and test actions
+    cy.get(".pf-c-breadcrumb__link").contains("Executions").click();
+    cy.get("td .pf-c-button.pf-m-link", { timeout: 10_000 }).eq(1).click();
+
+    cy.get(".pf-c-modal-box").should("have.length", 1);
+    cy.get(".pf-m-danger").contains("Delete").should("not.be.disabled");
+    cy.get(".pf-m-link").contains("Cancel").should("not.be.disabled");
+    cy.get(".pf-m-danger").contains("Delete").click();
+
+    cy.get(".pf-c-empty-state", { timeout: 10000 }).contains(
       "There are no analysis results for this project"
     );
   });
