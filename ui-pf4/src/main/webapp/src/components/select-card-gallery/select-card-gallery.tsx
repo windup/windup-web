6da8aee51fe28130a60f4fd90b869a6348f3724e
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-04-01 12:45:04 +0200

    Remove icons and change texts

--- ui-pf4/src/main/webapp/src/components/select-card-gallery/select-card-gallery.tsx
+++ ui-pf4/src/main/webapp/src/components/select-card-gallery/select-card-gallery.tsx
@@ -3,13 +3,13 @@ import { Gallery, GalleryItem } from "@patternfly/react-core";
 
 import { SelectCard } from "../select-card/select-card";
 
-import jbossLogo from "images/jboss.svg";
-import openshiftLogo from "images/openshift.svg";
-import linuxLogo from "images/linux.svg";
-import openjdkLogo from "images/openjdk.svg";
-import camelLogo from "images/camel.svg";
-import quarkusLogo from "images/quarkus.svg";
-import rhRuntimesLogo from "images/rh-runtimes.svg";
+// import jbossLogo from "images/jboss.svg";
+// import openshiftLogo from "images/openshift.svg";
+// import linuxLogo from "images/linux.svg";
+// import openjdkLogo from "images/openjdk.svg";
+// import camelLogo from "images/camel.svg";
+// import quarkusLogo from "images/quarkus.svg";
+// import rhRuntimesLogo from "images/rh-runtimes.svg";
 
 interface TransformationPathOption {
   label: string;
@@ -27,63 +27,84 @@ interface MultipleOptions {
 
 const options: TransformationPathOption[] = [
   {
-    label: "Application server migration to",
+    label: "Application server migration to EAP 7",
     description:
-      "Upgrade to the latest Release of JBoss EAP or migrate your applications to JBoss EAP from competitors' Enterprise Application Server (e.g. Oracle WebLogic Server).",
-    options: [
-      {
-        label: "JBoss EAP 6",
-        value: "eap6",
-        default: false,
-      },
-      {
-        label: "JBoss EAP 7",
-        value: "eap7",
-        default: true,
-      },
-    ],
-    iconSrc: jbossLogo,
+      "Upgrade to the latest Release of JBoss EAP or migrate your applications to JBoss EAP from competitors' Enterprise Application server (e.g.Oracle Weblogic Server).",
+    // options: [
+    //   {
+    //     label: "JBoss EAP 6",
+    //     value: "eap6",
+    //     default: false,
+    //   },
+    //   {
+    //     label: "JBoss EAP 7",
+    //     value: "eap7",
+    //     default: true,
+    //   },
+    // ],
+    options: "eap7",
+    // iconSrc: jbossLogo,
   },
   {
     label: "Containerization",
     description:
       "A comprehensive set of cloud and container readiness rules to assess applications for suitability for deployment on OpenShift Container Platform.",
     options: "cloud-readiness",
-    iconSrc: openshiftLogo,
-  },
-  {
-    label: "Linux",
-    description:
-      "Ensure there are no Microsoft Windows paths hard coded into your applications.",
-    options: "linux",
-    iconSrc: linuxLogo,
-  },
-  {
-    label: "OpenJDK",
-    description: "Rules to support the migration to OpenJDK from OracleJDK.",
-    options: "openjdk",
-    iconSrc: openjdkLogo,
-  },
-  {
-    label: "Camel",
-    description:
-      "A comprehensive set of rules for migration from Apache Camel 2 to Apache Camel 3.",
-    options: "camel",
-    iconSrc: camelLogo,
+    // iconSrc: openshiftLogo,
   },
   {
     label: "Quarkus",
     description:
-      "Rules to support the migration of Spring Boot applications to Quarkus.",
+      "Rules to support the migration of Spring Boot applications to Quarkus. The rules also identify other, non-Spring Boot,  technologies embedded within applications for which there are equivalent Quarkus extensions.",
     options: "quarkus",
-    iconSrc: quarkusLogo,
+    // iconSrc: quarkusLogo,
+  },
+  {
+    label: "OracleJDK to OpenJDK",
+    description: "Rules to support the migration to OpenJDK from OracleJDK.",
+    options: "openjdk",
+    // iconSrc: openjdkLogo,
+  },
+  {
+    label: "OpenJDK 11",
+    description: "Rules to support the migration to OpenJDK 11 from OpenJDK 8.",
+    options: "openjdk11",
+    // iconSrc: openjdkLogo,
+  },
+  {
+    label: "Linux",
+    description:
+      "Ensure that there are no Microsoft Windows paths hard coded and no Dynamic-Link Library (DLL) into your applications.",
+    options: "linux",
+    // iconSrc: linuxLogo,
+  },
+  {
+    label: "Jakarta EE 9",
+    description:
+      "A collection of rules to support migrating applications from Java EE 8 to Jakarta EE 9. The rules cover project dependencies, package renaming, updating XML Schema namespaces, the renaming of application configuration properties and bootstrapping files.",
+    options: "jakarta-ee",
+    // iconSrc: linuxLogo,
   },
   {
     label: "Spring Boot on Red Hat Runtimes",
     description:
-      "A set of rules for assessing the compatibility of applications against the versions of Spring Boot libraries supported by Red Hat Runtimes.",
+      "A set of rules for assessing the compatibility of applications against versions of Spring Boot libraries suported by Red Hat Runtimes.",
     options: "rhr",
-    iconSrc: rhRuntimesLogo,
+    // iconSrc: rhRuntimesLogo,
+  },
+  {
+    label: "Open Liberty",
+    description:
+      "A comprehensive set of rules for migrating traditional WebSphere applications to Open Liberty.",
+    options: "openliberty",
+    // iconSrc: rhRuntimesLogo,
+  },
+  {
+    label: "Camel",
+    description:
+      "Rules for the migration from Apache Camel 2 to Apache Camel 3.",
+    options: "camel",
+    // iconSrc: camelLogo,
   },
 ];
 
