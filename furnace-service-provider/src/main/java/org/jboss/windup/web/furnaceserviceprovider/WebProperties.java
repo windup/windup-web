d5c5c20b336ed809555a0e3c5602e9ced86f04ff
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2022-04-12 11:34:50 +0200

    [WINDUP-3333] - Web Console fails to start if any of the parent folders contains blank spaces (#791)
    * Fix Windows white space exception
    
    * Add If Windows OS condition
    
    * Fix spelling

--- furnace-service-provider/src/main/java/org/jboss/windup/web/furnaceserviceprovider/WebProperties.java
+++ furnace-service-provider/src/main/java/org/jboss/windup/web/furnaceserviceprovider/WebProperties.java
@@ -1,11 +1,13 @@
 package org.jboss.windup.web.furnaceserviceprovider;
 
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
 import org.jboss.vfs.VFS;
 import org.jboss.vfs.VirtualFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -126,6 +128,15 @@ public class WebProperties
             String pathString;
             if (pathUrl.toString().startsWith("vfs:"))
             {
+                // Below there are examples of different values for "pathUrl" depending on the OS where the Web Console is running:
+                // In Linux: pathUrl=vfs:/content/api.war/WEB-INF/lib/furnace-service-provider-5.3.0.Final.jar
+                // In Windows pathUrl=vfs:/C:/Users/myUsername/Downloads/mta-web/temp 1/content/api.war/WEB-INF/lib/furnace-service-provider-5.3.0.Final.jar
+                // In Windows: due to the fact that "pathUrl" contains the full path, there is the possibility that
+                // "pathUrl" might contain blank spaces which will generate an exception on "pathUrl.toURI()".
+                if (OperatingSystemUtils.isWindows() && pathUrl.toString().contains(" ")) {
+                    pathUrl = new URL(pathUrl.toString().replace(" ", "%20"));
+                }
+
                 VirtualFile virtualFile = VFS.getChild(pathUrl.toURI());
                 pathString = virtualFile.getParent().getParent().getParent().getPhysicalFile().getCanonicalPath();
                 LOG.info("From VFS - Path String: " + pathString);
