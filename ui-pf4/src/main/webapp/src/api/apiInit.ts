39aee0facd6b34658bb7ffea170bed8cea560905
Mark Brophy <36955467+m-brophy@users.noreply.github.com>
2024-05-08 11:45:56 +0100

    WINDUP-4139:axios upgrade to 0.28.0 (#975) (#982)
    * axios upgrade to 1.6.0
    
    * merge Carlos PR
    
    * install axios 1.6.0 properly
    
    * Use axios 0.28
    
    ---------
    
    Co-authored-by: carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
    (cherry picked from commit 1bae7a7fb2c34e1c3084a2e936f4cca0aba5a16a)

--- ui-pf4/src/main/webapp/src/api/apiInit.ts
+++ ui-pf4/src/main/webapp/src/api/apiInit.ts
@@ -10,6 +10,7 @@ export const initInterceptors = (getToken: () => Promise<string>) => {
   axios.interceptors.request.use(
     async (config) => {
       const token = await getToken();
+      if (!config.headers) config.headers = {};
       if (token) config.headers["Authorization"] = "Bearer " + token;
       return config;
     },
